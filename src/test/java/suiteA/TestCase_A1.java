96b2787ab28a452cd02642da0562845658928d36
Amneet Singh <amneet.singh@thomsonreuters.com>
2015-09-10 15:30:46 +0530

    TestCase_A1 changed

--- src/test/java/suiteA/TestCase_A1.java
+++ src/test/java/suiteA/TestCase_A1.java
@@ -154,7 +154,7 @@ public class TestCase_A1 extends TestBase{
 		ob.findElement(By.id(OR.getProperty("TR_email_textBox"))).sendKeys(email);
 		ob.findElement(By.id(OR.getProperty("TR_password_textBox"))).sendKeys(password);
 		ob.findElement(By.id(OR.getProperty("login_button"))).click();
-		Thread.sleep(15000);
+		Thread.sleep(25000);
 		if(!checkElementPresence("help_link")){
 			
 			test.log(LogStatus.FAIL, "Newly registered user credentials are not working fine");//extent reports
