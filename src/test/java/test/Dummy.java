19bb2b8a57a17c6d832223f1edb04780ee831ad0
Amneet Singh <amneet.singh@thomsonreuters.com>
2015-09-14 12:05:26 +0530

    Took Chinna's latest code

--- src/test/java/test/Dummy.java
+++ src/test/java/test/Dummy.java
@@ -2,4 +2,9 @@ package test;
 
 public class Dummy {
 
+	public static void main(String[] args){
+		
+		String text="hhurezoarjygylxehglmwsznysgqdnznuyfdntdfszutzweplwkpanweytnoiwbvwhtvef";
+		System.out.println(text.length());
+	}
 }
