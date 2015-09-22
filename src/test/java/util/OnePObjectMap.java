703f110d1b8f236a1bc1a3d9e863f88de0df7636
chinna putha <chinna.putha@thomsonreuters.com>
2015-09-22 18:34:46 +0530

    added following new profile script 1.Profile Interest and Skills Update

--- src/test/java/util/OnePObjectMap.java
+++ src/test/java/util/OnePObjectMap.java
@@ -13,7 +13,9 @@ public enum OnePObjectMap {
 	HOME_PROJECT_NEON_APP_RECORD_VIEW_DETALIS_XPATH("//a[contains(text(),'Details')]"),
 	HOME_PROJECT_NEON_APP_RECORD_VIEW_DETALIS_BACKTOPN_CSS("a[title='Back to Project Neon']"),
 	HOME_PROJECT_NEON_OWN_PROFILE_COMMENTS_LIKE_CSS("span[class^='webui-icon webui-icon-like']"),
-	HOME_PROJECT_NEON_APP_PROFILE_COMMENTS_CSS("li[class='search-heading tabs ng-isolate-scope active']");
+	HOME_PROJECT_NEON_APP_PROFILE_COMMENTS_CSS("li[class='search-heading tabs ng-isolate-scope active']"),
+	HOME_PROJECT_NEON_OWN_PROFILE_INTEREST_SKILLS_CSS("a[class='remove-button ng-binding ng-scope']");
+	
 	
 	private String locator;
 
