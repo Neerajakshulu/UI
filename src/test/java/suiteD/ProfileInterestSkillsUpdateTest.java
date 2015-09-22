f36f154030c3f297ff2a54e9b491c98f66a128be
chinna putha <chinna.putha@thomsonreuters.com>
2015-09-22 18:44:14 +0530

    added profileInterestSkillsUpdateTest.java

---
+++ src/test/java/suiteD/ProfileInterestSkillsUpdateTest.java
@@ -0,0 +1,162 @@
+package suiteD;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import base.TestBase;
+import suiteC.LoginTR;
+import util.BrowserAction;
+import util.BrowserWaits;
+import util.ErrorUtil;
+import util.OnePObjectMap;
+import util.TestUtil;
+
+public class ProfileInterestSkillsUpdateTest extends TestBase {
+	
+	String runmodes[]=null;
+	static int count=-1;
+	
+	static boolean fail=false;
+	static boolean skip=false;
+	static int status=1;
+	
+	@BeforeTest
+	public void beforeTest() {
+		test = extent.startTest(this.getClass().getSimpleName(),
+				"Validate User Own Profile Interests and Skills Update Test").assignCategory("Suite D");
+		runmodes=TestUtil.getDataSetRunmodes(suiteDxls, this.getClass().getSimpleName());
+	}
+	
+	/**
+	 * Method for wait TR Login Screen		
+	 * @throws Exception, When TR Login screen not displayed
+	 */
+	@Test
+	@Parameters({"username","password"})
+	public void testLoginTRAccount(String username,String password) throws Exception  {
+		
+		boolean suiteRunmode=TestUtil.isSuiteRunnable(suiteXls, "D Suite");
+		boolean testRunmode=TestUtil.isTestCaseRunnable(suiteDxls,this.getClass().getSimpleName());
+		boolean master_condition=suiteRunmode && testRunmode;
+		
+		if(!master_condition) {
+			status=3;
+			test.log(LogStatus.SKIP, "Skipping test case "+this.getClass().getSimpleName()+" as the run mode is set to NO");
+			throw new SkipException("Skipping Test Case"+this.getClass().getSimpleName()+" as runmode set to NO");//reports
+		}
+		
+		
+		// test the runmode of current dataset
+		count++;
+		if(!runmodes[count].equalsIgnoreCase("Y")) {
+			test.log(LogStatus.INFO, "Runmode for test set data set to no "+count);
+			skip=true;
+			throw new SkipException("Runmode for test set data set to no "+count);
+		}
+		test.log(LogStatus.INFO,this.getClass().getSimpleName()+" execution starts for data set #"+ count+"--->");
+		
+				try {
+					openBrowser();
+					clearCookies();
+					maximizeWindow();
+					
+					ob.navigate().to(System.getProperty("host"));
+					LoginTR.waitForTRHomePage();
+					Thread.sleep(6000);
+					LoginTR.enterTRCredentials(username, password);
+					LoginTR.clickLogin();
+				} catch (Throwable t) {
+					test.log(LogStatus.FAIL,"Something Unexpected");
+					//print full stack trace
+					StringWriter errors = new StringWriter();
+					t.printStackTrace(new PrintWriter(errors));
+					test.log(LogStatus.INFO,errors.toString());
+					ErrorUtil.addVerificationFailure(t);
+					status=2;//excel
+					test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName()+"_TR_Login_Not_happended")));
+					closeBrowser();
+				}
+	}
+	
+	
+	@Test(dependsOnMethods="testLoginTRAccount")
+	@Parameters({"maxInterstSkilChar","finalInterstSkilChar"})
+	public void updateProfileInterestsAndSkills(int maxInterstSkilChar,int finalInterstSkilChar) throws Exception  {
+			try {
+				profileSkillsUpdation(maxInterstSkilChar,finalInterstSkilChar);
+				test.log(LogStatus.INFO,this.getClass().getSimpleName()+" Test execution ends ");
+				LoginTR.logOutApp();
+				closeBrowser();
+			} catch (Throwable t) {
+				test.log(LogStatus.FAIL,"Something Unexpected");
+				//print full stack trace
+				StringWriter errors = new StringWriter();
+				t.printStackTrace(new PrintWriter(errors));
+				test.log(LogStatus.INFO,errors.toString());
+				ErrorUtil.addVerificationFailure(t);
+				status=2;//excel
+				test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName()+"_profile_Interests and Skills not Updated")));
+				closeBrowser();
+			}
+	}
+	
+	
+	/**
+	 * Method for user Profile comments like
+	 * @throws Exception, when comments like is not working as expected
+	 */
+	
+	public void profileSkillsUpdation(int maxInterstSkilChar,int finalInterstSkilChar) throws Exception  {
+		ob.findElement(By.cssSelector(OR.getProperty("tr_profile_dropdown_css"))).click();
+		BrowserWaits.waitUntilText("Profile");
+		ob.findElement(By.linkText(OR.getProperty("tr_profile_link"))).click();
+		Thread.sleep(4000);
+		
+		List<WebElement> skills=BrowserAction.getElements(OnePObjectMap.HOME_PROJECT_NEON_OWN_PROFILE_INTEREST_SKILLS_CSS);
+		//System.out.println("skills size-->"+skills.size());
+		if(skills.size()>0) {
+			//existing interests and skills are available
+			for(WebElement skill:skills){
+				skill.click();
+				Thread.sleep(2000);
+			}
+		}
+		String statusBeforeSkill=ob.findElement(By.cssSelector("div[class='row interests-and-skills']")).findElement(By.xpath("tags-input/div/div/input")).getAttribute("class");
+		//System.out.println("Attr stauts-->"+statusBeforeSkill);
+		ob.findElement(By.cssSelector("div[class='row interests-and-skills']")).findElement(By.xpath("tags-input/div/div/input")).sendKeys(RandomStringUtils.randomAlphabetic(maxInterstSkilChar));
+		ob.findElement(By.cssSelector("div[class='row interests-and-skills']")).findElement(By.xpath("tags-input/div/div/input")).sendKeys(Keys.ENTER);
+		String statusAfterSkill=ob.findElement(By.cssSelector("div[class='row interests-and-skills']")).findElement(By.xpath("tags-input/div/div/input")).getAttribute("class");
+		Thread.sleep(4000);
+		//System.out.println("Attr stauts-->"+statusAfterSkill);
+		Assert.assertNotEquals(statusBeforeSkill, statusAfterSkill);
+		ob.findElement(By.cssSelector("div[class='row interests-and-skills']")).findElement(By.xpath("tags-input/div/div/input")).clear();
+		ob.findElement(By.cssSelector("div[class='row interests-and-skills']")).findElement(By.xpath("tags-input/div/div/input")).sendKeys(RandomStringUtils.randomAlphabetic(finalInterstSkilChar));
+		Thread.sleep(4000);
+		
+	}
+	
+	@AfterTest
+	public void reportTestResult() {
+		extent.endTest(test);
+		if(status==1)
+			TestUtil.reportDataSetResult(suiteDxls, "Test Cases", TestUtil.getRowNum(suiteDxls,this.getClass().getSimpleName()), "PASS");
+		else if(status==2)
+			TestUtil.reportDataSetResult(suiteDxls, "Test Cases", TestUtil.getRowNum(suiteDxls,this.getClass().getSimpleName()), "FAIL");
+		else
+			TestUtil.reportDataSetResult(suiteDxls, "Test Cases", TestUtil.getRowNum(suiteDxls,this.getClass().getSimpleName()), "SKIP");
+	}
+}
