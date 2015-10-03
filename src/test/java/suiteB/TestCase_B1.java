e97547e1f71da4587657d8005458ee142fdfc97a
Amneet Singh <amneet.singh@thomsonreuters.com>
2015-10-03 22:31:34 +0530

    Running B1

--- src/test/java/suiteB/TestCase_B1.java
+++ src/test/java/suiteB/TestCase_B1.java
@@ -104,7 +104,7 @@ public class TestCase_B1 extends TestBase{
 				WebElement myE=ob.findElement(By.xpath(OR.getProperty("details_link")));
 				JavascriptExecutor executor = (JavascriptExecutor)ob;
 				executor.executeScript("arguments[0].click();", myE);
-				Thread.sleep(15000);
+				
 				
 				Set<String> myset=ob.getWindowHandles();
 				Iterator<String> myIT=myset.iterator();
@@ -118,7 +118,7 @@ public class TestCase_B1 extends TestBase{
 				}
 				
 				ob.switchTo().window(mylist55.get(1));
-				
+				Thread.sleep(15000);
 				
 				pageText=ob.getPageSource().toLowerCase();
 				condition1=pageText.contains("cat");
