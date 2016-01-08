f9a18dbfa3b11c7ff3128c1febcac59c730e12f6
Kavya Revanna <kavya.revanna@thomsonreuters.com>
2016-01-08 14:35:45 +0530

    Added new tests for Authoring-Post

--- src/test/java/pages/PostRecordViewPage.java
+++ src/test/java/pages/PostRecordViewPage.java
@@ -1,6 +1,10 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import com.relevantcodes.extentreports.LogStatus;
 
 import base.TestBase;
 import util.OnePObjectMap;
@@ -66,8 +70,8 @@ public class PostRecordViewPage extends TestBase{
 	 */
 	public static void clickOnEditButton(){
 		waitForElementTobeVisible(ob,
-				By.cssSelector(OnePObjectMap.HOME_PROJECT_NEON_VIEW_POST_WATCH_CSS.toString()), 180);
-		ob.findElement(By.cssSelector(OnePObjectMap.HOME_PROJECT_NEON_VIEW_POST_WATCH_CSS.toString()))
+				By.cssSelector(OnePObjectMap.HOME_PROJECT_NEON_VIEW_POST_EDIT_CSS.toString()), 180);
+		ob.findElement(By.cssSelector(OnePObjectMap.HOME_PROJECT_NEON_VIEW_POST_EDIT_CSS.toString()))
 				.click();
 	}
 	
@@ -81,6 +85,56 @@ public class PostRecordViewPage extends TestBase{
 				.click();
 	}
 	
-	
-	
+	public static boolean validateLikeAndUnlikePostActions() throws InterruptedException {
+		boolean result;
+		WebElement appreciationButton;
+		int countBefore, countAfter;
+		waitForAjax(ob);
+		waitForElementTobeVisible(ob,
+				By.cssSelector(OnePObjectMap.HOME_PROJECT_NEON_VIEW_POST_APPRECIATION_COUNT_CSS.toString()), 180);
+		countBefore = Integer.parseInt(ob
+				.findElement(
+						By.cssSelector(OnePObjectMap.HOME_PROJECT_NEON_VIEW_POST_APPRECIATION_COUNT_CSS.toString()))
+				.getText());
+		appreciationButton = ob
+				.findElement(By.cssSelector(OnePObjectMap.HOME_PROJECT_NEON_VIEW_POST_APPRECIATION_CSS.toString()));
+
+		if (appreciationButton.getAttribute("event-action").equalsIgnoreCase("like")) {
+			appreciationButton.click();
+			Thread.sleep(5000);
+			countAfter = Integer.parseInt(ob
+					.findElement(
+							By.cssSelector(OnePObjectMap.HOME_PROJECT_NEON_VIEW_POST_APPRECIATION_COUNT_CSS.toString()))
+					.getText());
+			try {
+				Assert.assertEquals(countAfter, countBefore + 1);
+				test.log(LogStatus.PASS, "Appreciation action on post is working as expected");
+				result = true;
+			} catch (Throwable e) {
+				result = false;
+				test.log(LogStatus.FAIL, "Appreciation action on post is not working as expected");
+			}
+
+		} else {
+			appreciationButton.click();
+			Thread.sleep(5000);
+			countAfter = Integer.parseInt(ob
+					.findElement(
+							By.cssSelector(OnePObjectMap.HOME_PROJECT_NEON_VIEW_POST_APPRECIATION_COUNT_CSS.toString()))
+					.getText());
+			try {
+				Assert.assertEquals(countAfter, countBefore - 1);
+				test.log(LogStatus.PASS, "un Appreciation action on post is working as expected");
+				result = true;
+			} catch (Throwable e) {
+				result = false;
+				test.log(LogStatus.FAIL, "un Appreciation action on post is not working as expected");
+			}
+
+		}
+
+		return result;
+
+	}
+
 }
