413fc888504808563252c20d6647b3652bbeaae0
Sachin Bennur <sachin.bennur@thomsonreuters.com>
2016-03-14 15:07:12 +0530

    Notification update

---
+++ src/test/java/suiteF/TestCase_F10.java
@@ -0,0 +1,171 @@
+package suiteF;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import suiteC.LoginTR;
+import util.ErrorUtil;
+import util.TestUtil;
+import base.TestBase;
+
+public class TestCase_F10 extends TestBase {
+	static int status = 1;
+
+	// Following is the list of status:
+	// 1--->PASS
+	// 2--->FAIL
+	// 3--->SKIP
+	// Checking whether this test case should be skipped or not
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		String var = xlRead(returnExcelPath(this.getClass().getSimpleName().charAt(9)),
+				Integer.parseInt(this.getClass().getSimpleName().substring(10) + ""), 1);
+		test = extent.startTest(var, "Verify that user receives a notification when someone comments on an post contained in his watchlist")
+				.assignCategory("Suite F");
+
+	}
+	
+	@Test
+	public void testcaseF10() throws Exception {
+		boolean suiteRunmode = TestUtil.isSuiteRunnable(suiteXls, "F Suite");
+		boolean testRunmode = TestUtil.isTestCaseRunnable(suiteFxls, this.getClass().getSimpleName());
+		boolean master_condition = suiteRunmode && testRunmode;
+
+		if (!master_condition) {
+
+			status = 3;// excel
+			test.log(LogStatus.SKIP,
+					"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
+			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
+
+		}
+
+		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts--->");
+		try{
+			openBrowser();
+			maximizeWindow();
+			clearCookies();
+			
+			ob.navigate().to(host);
+			LoginTR.enterTRCredentials(user1, CONFIG.getProperty("defaultPassword"));
+			LoginTR.clickLogin();
+			//searching for posts
+			LoginTR.searchArticle("POST for");
+			
+			JavascriptExecutor jse = (JavascriptExecutor) ob;
+			jse.executeScript("scroll(0,-500)");
+			//Thread.sleep(2000);
+			waitForElementTobeVisible(ob,By.xpath(OR.getProperty("search_results_posts_tab_link")), 30);
+			test.log(LogStatus.INFO,"Watching a post");
+			ob.findElement(By.xpath(OR.getProperty("search_results_posts_tab_link"))).click();
+			//Thread.sleep(5000);
+			waitForElementTobeVisible(ob,By.xpath(OR.getProperty("search_results_post_link")), 30);
+			String postWatched=ob.findElement(By.xpath(OR.getProperty("search_results_post_link"))).getText();
+			ob.findElement(By.xpath(OR.getProperty("search_watchlist_image"))).click();
+			//Thread.sleep(3000);
+			waitForElementTobeVisible(ob,By.xpath(OR.getProperty("selectWatchListInBucket")), 30);
+			ob.findElement(By.xpath(OR.getProperty("selectWatchListInBucket"))).click();
+			//Thread.sleep(5000);
+			waitForElementTobeVisible(ob,By.xpath(OR.getProperty("closeWatchListBucketDisplay")), 30);
+			ob.findElement(By.xpath(OR.getProperty("closeWatchListBucketDisplay"))).click();
+			//Thread.sleep(1000);
+			test.log(LogStatus.INFO,"Post added to user's watchList");
+			Thread.sleep(3000);
+			LoginTR.logOutApp();
+			//create a new user and comment on the same post in user1's watchlist
+			
+			fn3 = generateRandomName(8);
+			ln3 = generateRandomName(10);
+			System.out.println(fn3 + " " + ln3);
+			user3 = createNewUser(fn3, ln3);
+			//Thread.sleep(5000);
+			//searching the post and commenting on it
+			ob.findElement(By.xpath(OR.getProperty("searchBox_textBox"))).sendKeys(postWatched);
+			ob.findElement(By.xpath(OR.getProperty("search_button"))).click();
+			Thread.sleep(4000);
+			
+			jse.executeScript("scroll(0,-500)");
+			//Thread.sleep(2000);
+			waitForElementTobeVisible(ob,By.xpath(OR.getProperty("search_results_posts_tab_link")), 30);
+			test.log(LogStatus.INFO,"Commenting on a post");
+			ob.findElement(By.xpath(OR.getProperty("search_results_posts_tab_link"))).click();
+			//Thread.sleep(5000);
+			waitForElementTobeVisible(ob,By.xpath(OR.getProperty("search_results_post_link")), 30);
+			ob.findElement(By.xpath(OR.getProperty("search_results_post_link"))).click();
+			//Thread.sleep(4000);
+			waitForElementTobeVisible(ob,By.xpath(OR.getProperty("document_comment_textbox")), 30);
+			ob.findElement(By.xpath(OR.getProperty("document_comment_textbox"))).sendKeys("TestCase_F10:Post in other user's watchlist");
+			Thread.sleep(3000);
+			waitForElementTobeVisible(ob,By.xpath(OR.getProperty("document_addComment_button")), 30);
+			Thread.sleep(3000);
+			jsClick(ob, ob.findElement(By.xpath(OR.getProperty("document_addComment_button"))));
+			Thread.sleep(2000);
+			LoginTR.logOutApp();
+			
+			//Login with user1 and check for notification
+			LoginTR.enterTRCredentials(user1, CONFIG.getProperty("defaultPassword"));
+			LoginTR.clickLogin();
+			
+			test.log(LogStatus.INFO,"Checking if Notification is received");
+			String text = ob.findElement(By.xpath(OR.getProperty("notificationDocumentComment"))).getText();
+			System.out.println(text);
+		
+			try {
+				Assert.assertTrue(text.contains("New comments") && text.contains(postWatched)
+						&& text.contains(fn3 + " " + ln3) && text.contains("TestCase_F10:Post in other user's watchlist")
+						&& text.contains("TODAY"));
+				test.log(LogStatus.PASS, "User receiving notification with correct content");
+			} catch (Throwable t) {
+
+				test.log(LogStatus.FAIL, "User receiving notification with incorrect content");// extent
+				// reports
+				test.log(LogStatus.INFO, "Error--->" + t);
+				ErrorUtil.addVerificationFailure(t);
+				status = 2;// excel
+				test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(captureScreenshot(
+						this.getClass().getSimpleName() + "_user_receiving_notification_with_incorrect_content")));// screenshot
+				closeBrowser();
+
+			}
+			closeBrowser();
+			
+			
+			
+		}catch(Throwable t){
+			test.log(LogStatus.FAIL, "User receiving notification with incorrect content");// extent
+			// reports
+			test.log(LogStatus.INFO, "Error--->" + t);
+			ErrorUtil.addVerificationFailure(t);
+			status = 2;// excel
+			test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName() + "_user_receiving_notification_with_incorrect_content")));// screenshot
+			closeBrowser();
+		}
+		
+		
+	}
+	
+	
+	@AfterTest
+	public void reportTestResult() {
+		extent.endTest(test);
+
+		if (status == 1)
+			TestUtil.reportDataSetResult(suiteFxls, "Test Cases",
+					TestUtil.getRowNum(suiteFxls, this.getClass().getSimpleName()), "PASS");
+		else if (status == 2)
+			TestUtil.reportDataSetResult(suiteFxls, "Test Cases",
+					TestUtil.getRowNum(suiteFxls, this.getClass().getSimpleName()), "FAIL");
+		else
+			TestUtil.reportDataSetResult(suiteFxls, "Test Cases",
+					TestUtil.getRowNum(suiteFxls, this.getClass().getSimpleName()), "SKIP");
+
+	}
+	
+}
---
+++ src/test/java/suiteF/TestCase_F15.java
@@ -0,0 +1,116 @@
+package suiteF;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import suiteC.LoginTR;
+import util.ErrorUtil;
+import util.TestUtil;
+import base.TestBase;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+public class TestCase_F15 extends TestBase {
+	static int status = 1;
+	// Following is the list of status:
+		// 1--->PASS
+		// 2--->FAIL
+		// 3--->SKIP
+		// Checking whether this test case should be skipped or not
+		@BeforeTest
+		public void beforeTest() throws Exception {
+			String var = xlRead(returnExcelPath(this.getClass().getSimpleName().charAt(9)),
+					Integer.parseInt(this.getClass().getSimpleName().substring(10) + ""), 1);
+			test = extent.startTest(var, "Verify that users should be able to select from a list of suggested topics and check selected topic is presented in users type ahead")
+					.assignCategory("Suite F");
+
+		}
+		
+		@Test
+		public void testcaseF15() throws Exception {
+			boolean suiteRunmode = TestUtil.isSuiteRunnable(suiteXls, "F Suite");
+			boolean testRunmode = TestUtil.isTestCaseRunnable(suiteFxls, this.getClass().getSimpleName());
+			boolean master_condition = suiteRunmode && testRunmode;
+
+			if (!master_condition) {
+
+				status = 3;// excel
+				test.log(LogStatus.SKIP,
+						"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
+				throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
+
+			}
+
+			test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts--->");
+			try{
+				openBrowser();
+				maximizeWindow();
+				clearCookies();
+				ob.navigate().to(host);
+				//Logging in with User2
+				LoginTR.enterTRCredentials(user1, CONFIG.getProperty("defaultPassword"));
+				LoginTR.clickLogin();
+				//Thread.sleep(8000);
+				waitForElementTobeVisible(ob,By.xpath(OR.getProperty("trending_now_menu_links").replaceAll("FILTER_TYPE","Topics")), 30);
+				jsClick(ob,ob.findElement(By.xpath(OR.getProperty("trending_now_menu_links").replaceAll("FILTER_TYPE","Topics"))));
+				//Thread.sleep(6000);
+				waitForElementTobeVisible(ob,By.xpath(OR.getProperty("trending_now_topics_link")), 30);
+				WebElement element=ob.findElement(By.xpath(OR.getProperty("trending_now_topics_link")));
+				String specialCharacterRemovedoutput=element.getText().replaceAll("[^\\dA-Za-z ]","");
+				String expectedTitle=specialCharacterRemovedoutput.replaceAll("( )+"," ");
+				element.click();
+				Thread.sleep(4000);
+				waitForElementTobeVisible(ob,By.xpath("//input[@type='text']"), 30);
+				String searchText=ob.findElement(By.xpath("//input[@type='text']")).getAttribute("value");
+				System.out.println(searchText);
+
+				try {
+					Assert.assertTrue(searchText.equals(expectedTitle));
+					test.log(LogStatus.PASS, "User receiving notification with correct content");
+				} catch (Throwable t) {
+
+					test.log(LogStatus.FAIL, "Title selected is not same in search text box");// extent
+					// reports
+					test.log(LogStatus.INFO, "Error--->" + t);
+					ErrorUtil.addVerificationFailure(t);
+					status = 2;// excel
+					test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(captureScreenshot(
+							this.getClass().getSimpleName() + "_Title selected is not same in search text box")));// screenshot
+					closeBrowser();
+				}
+
+				closeBrowser();
+				
+			}catch(Throwable t){
+				test.log(LogStatus.FAIL, "Something happened");// extent
+				// reports
+				test.log(LogStatus.INFO, "Error--->" + t);
+				ErrorUtil.addVerificationFailure(t);
+				status = 2;// excel
+				test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(captureScreenshot(
+						this.getClass().getSimpleName() + "_Title selected is not same in search text box")));// screenshot
+				closeBrowser();
+			}
+		}
+		
+		@AfterTest
+		public void reportTestResult() {
+			extent.endTest(test);
+
+			if (status == 1)
+				TestUtil.reportDataSetResult(suiteFxls, "Test Cases",
+						TestUtil.getRowNum(suiteFxls, this.getClass().getSimpleName()), "PASS");
+			else if (status == 2)
+				TestUtil.reportDataSetResult(suiteFxls, "Test Cases",
+						TestUtil.getRowNum(suiteFxls, this.getClass().getSimpleName()), "FAIL");
+			else
+				TestUtil.reportDataSetResult(suiteFxls, "Test Cases",
+						TestUtil.getRowNum(suiteFxls, this.getClass().getSimpleName()), "SKIP");
+
+		}
+}
