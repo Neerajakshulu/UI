6684b2d6ca7cf4edd63968882e4d6dbeaf04d5c6
UC196983 <UC196983@UC196983-TPD-A.ten.thomsonreuters.com>
2016-03-15 18:32:24 +0530

    Fixes for TestCase_E31-TestCase_E41

--- src/test/java/suiteE/TestCase_E38.java
+++ src/test/java/suiteE/TestCase_E38.java
@@ -15,7 +15,6 @@ import org.testng.annotations.Test;
 import com.relevantcodes.extentreports.LogStatus;
 
 import base.TestBase;
-import suiteC.LoginTR;
 import util.ErrorUtil;
 import util.TestUtil;
 
@@ -57,62 +56,52 @@ public class TestCase_E38 extends TestBase {
 		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts--->");
 		try {
 
-			// String search_query = "biology";
-
-			// 1)Create User1 and logout
+			// 1) Login as user2
 			openBrowser();
 			maximizeWindow();
 			clearCookies();
-			fn1 = generateRandomName(8);
-			ln1 = generateRandomName(10);
-			System.out.println(fn1 + " " + ln1);
-			user1 = createNewUser(fn1, ln1);
+			ob.get(host);
+			loginAsSpecifiedUser(user2, CONFIG.getProperty("defaultPassword"));
 			// Navigate to the watch list landing page
 			ob.findElement(By.xpath(OR.getProperty("watchlist_link"))).click();
+			waitForPageLoad(ob);
 			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("createWatchListButton")), 30);
-			// Creating public watch list
-			String newWatchlistName = "New Watchlist";
+			// Creating two public watch list
+			String newWatchlistName = "WatchlistToBeDeleted";
 			for (int i = 1; i <= 2; i++) {
-				// waitForElementTobeClickable(ob,
-				// By.xpath(OR.getProperty("createWatchListButton")), 30);
+				waitForElementTobeVisible(ob, By.xpath(OR.getProperty("createWatchListButton")), 30);
 				ob.findElement(By.xpath(OR.getProperty("createWatchListButton"))).click();
-				waitForElementTobeVisible(ob, By.xpath("//div[@data-submit-callback='Workspace.submitWatchlistForm']"),
-						30);
+				waitForElementTobeVisible(ob, By.xpath(OR.getProperty("newWatchListNameTextBox")), 30);
 				ob.findElement(By.xpath(OR.getProperty("newWatchListNameTextBox"))).sendKeys(newWatchlistName + i);
+				waitForElementTobeVisible(ob, By.xpath(OR.getProperty("newWatchListDescriptionTextArea")), 30);
 				ob.findElement(By.xpath(OR.getProperty("newWatchListDescriptionTextArea")))
-						.sendKeys("This is my newly created watch list");
-				ob.findElement(By.xpath(OR.getProperty("newWatchListPublicCheckBox"))).click();
-				// Clicking on Create button
+						.sendKeys("This is my newly created watch list.");
+				waitForElementTobeClickable(ob, By.xpath(OR.getProperty("newWatchListPublicCheckBox")), 30);
+				jsClick(ob, ob.findElement(By.xpath(OR.getProperty("newWatchListPublicCheckBox"))));
+				waitForElementTobeClickable(ob, By.xpath(OR.getProperty("newWatchListCreateButton")), 30);
 				ob.findElement(By.xpath(OR.getProperty("newWatchListCreateButton"))).click();
-				Thread.sleep(4000);
+				waitForElementTobeVisible(ob, By.xpath("//a[contains(text(),'" + newWatchlistName + i + "')]"), 30);
 			}
 
 			// Deleting the first watch list
-			ob.findElement(By.xpath(OR.getProperty("watchlist_name"))).click();
-			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("delete_button_image")), 30);
-			ob.findElement(By.xpath(OR.getProperty("delete_button_image"))).click();
-			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("delete_watchlist_popup")), 4);
-			waitForElementTobeClickable(ob, By.xpath(OR.getProperty("delete_button_in_popup")), 2);
-			ob.findElement(By.xpath(OR.getProperty("delete_button_in_popup"))).click();
-			Thread.sleep(4000);
-			LoginTR.logOutApp();
+			deleteFirstWatchlist();
 			closeBrowser();
-			// 2)Login as User2 and navigate to the user1 profile page
+			// 2)Login as User1 and navigate to the user2 profile page
 			openBrowser();
 			maximizeWindow();
 			clearCookies();
 			ob.navigate().to(host);
-			LoginTR.enterTRCredentials("Prasenjit.Patra@thomsonreuters.com", "Techm@2015");
-			LoginTR.clickLogin();
+			loginAsSpecifiedUser(user1, CONFIG.getProperty("defaultPassword"));
 			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("searchBox_textBox")), 30);
 			// Searching for article
 			selectSearchTypeFromDropDown("People");
-			ob.findElement(By.xpath(OR.getProperty("searchBox_textBox"))).sendKeys(fn1 + " " + ln1);
+			ob.findElement(By.xpath(OR.getProperty("searchBox_textBox"))).sendKeys(fn2 + " " + ln2);
 			ob.findElement(By.xpath(OR.getProperty("search_button"))).click();
-			waitForElementTobeVisible(ob, By.linkText(fn1 + " " + ln1), 30);
+			waitForElementTobeVisible(ob, By.linkText(fn2 + " " + ln2), 30);
 			// Navigating to the first user profile page
 			// ob.findElement(By.xpath("//a[@event-category='searchresult-ck-profile']")).click();
-			ob.findElement(By.linkText(fn1 + " " + ln1)).click();
+			ob.findElement(By.linkText(fn2 + " " + ln2)).click();
+			waitForPageLoad(ob);
 			waitForElementTobeClickable(ob, By.xpath(OR.getProperty("tr_watchlists_tab_in_profile_page")), 60);
 			// Navigating to the watch list tab
 			ob.findElement(By.xpath(OR.getProperty("tr_watchlists_tab_in_profile_page"))).click();
