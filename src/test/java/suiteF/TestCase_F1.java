7545023305980fc7258f5c0e5ea06563cb4324be
Sachin Bennur <sachin.bennur@thomsonreuters.com>
2016-04-11 18:50:19 +0530

    changes in F1 and F2 test cases

--- src/test/java/suiteF/TestCase_F1.java
+++ src/test/java/suiteF/TestCase_F1.java
@@ -47,86 +47,88 @@ public class TestCase_F1 extends TestBase {
 
 	@Test
 	public void testcaseF1() throws Exception {
+		
+		Assert.assertEquals("hjhgj", "ngfjhfjhg");
 
-		boolean suiteRunmode = TestUtil.isSuiteRunnable(suiteXls, "F Suite");
-		boolean testRunmode = TestUtil.isTestCaseRunnable(suiteFxls, this.getClass().getSimpleName());
-		boolean master_condition = suiteRunmode && testRunmode;
-
-		if (!master_condition) {
-
-			status = 3;// excel
-			test.log(LogStatus.SKIP,
-					"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
-			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
-
-		}
-
-		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts--->");
-		try {
-			// 1)Create User1 and logout
-			openBrowser();
-			maximizeWindow();
-			clearCookies();
-
-			ob.navigate().to(host);
-//			ob.navigate().to(CONFIG.getProperty("testSiteName"));
-			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("TR_login_button")),20);
-
-//			pf.getLoginTRInstance(ob).enterTRCredentials(user2, CONFIG.getProperty("defaultPassword"));
-//			pf.getLoginTRInstance(ob).clickLogin();
-			
-//			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("searchBox_textBox")),30);
-//			ob.findElement(By.xpath(OR.getProperty("searchBox_textBox"))).sendKeys(fn1 + " " + ln1);
-//			ob.findElement(By.xpath(OR.getProperty("search_button"))).click();
+//		boolean suiteRunmode = TestUtil.isSuiteRunnable(suiteXls, "F Suite");
+//		boolean testRunmode = TestUtil.isTestCaseRunnable(suiteFxls, this.getClass().getSimpleName());
+//		boolean master_condition = suiteRunmode && testRunmode;
 //
-//			JavascriptExecutor jse = (JavascriptExecutor) ob;
-//			jse.executeScript("scroll(0,-500)");
-//			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("profilesTabHeading_link")),30);
-//			ob.findElement(By.xpath(OR.getProperty("profilesTabHeading_link"))).click();
-//			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("search_follow_button")), 40);
-//			ob.findElement(By.xpath(OR.getProperty("search_follow_button"))).click();
-//			Thread.sleep(3000);
-//			logout();
-
-			// 3)Verify that User1 receives a notification with correct data
-			pf.getLoginTRInstance(ob).enterTRCredentials(user1, CONFIG.getProperty("defaultPassword"));
-			pf.getLoginTRInstance(ob).clickLogin();
-			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("notification")),30);
-			String text = ob.findElement(By.xpath(OR.getProperty("notification"))).getText();
-			System.out.println(text);
-			
-			try {
-				Assert.assertTrue(/*text.contains("TODAY") &&*/ text.contains(fn2 + " " + ln2) && text.contains("is now following you"));
-				test.log(LogStatus.PASS, "User receiving notification with correct content");
-			} catch (Throwable t) {
-
-				test.log(LogStatus.FAIL, "User receiving notification with incorrect content");// extent
-																								// reports
-				test.log(LogStatus.INFO, "Error--->" + t);
-				ErrorUtil.addVerificationFailure(t);
-				status = 2;// excel
-				test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(captureScreenshot(
-						this.getClass().getSimpleName() + "_user_receiving_notification_with_incorrect_content")));// screenshot
-
-			}
-
-			closeBrowser();
-
-		} catch (Throwable t) {
-			test.log(LogStatus.FAIL, "Something unexpected happened");// extent
-																		// reports
-			// next 3 lines to print whole testng error in report
-			StringWriter errors = new StringWriter();
-			t.printStackTrace(new PrintWriter(errors));
-			test.log(LogStatus.INFO, errors.toString());// extent reports
-			ErrorUtil.addVerificationFailure(t);// testng
-			status = 2;// excel
-			test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(
-					captureScreenshot(this.getClass().getSimpleName() + "_something_unexpected_happened")));// screenshot
-			closeBrowser();
-		}
-
-		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution ends--->");
+//		if (!master_condition) {
+//
+//			status = 3;// excel
+//			test.log(LogStatus.SKIP,
+//					"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
+//			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
+//
+//		}
+//
+//		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts--->");
+//		try {
+//			// 1)Create User1 and logout
+//			openBrowser();
+//			maximizeWindow();
+//			clearCookies();
+//
+//			ob.navigate().to(host);
+////			ob.navigate().to(CONFIG.getProperty("testSiteName"));
+//			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("TR_login_button")),20);
+//
+////			pf.getLoginTRInstance(ob).enterTRCredentials(user2, CONFIG.getProperty("defaultPassword"));
+////			pf.getLoginTRInstance(ob).clickLogin();
+//			
+////			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("searchBox_textBox")),30);
+////			ob.findElement(By.xpath(OR.getProperty("searchBox_textBox"))).sendKeys(fn1 + " " + ln1);
+////			ob.findElement(By.xpath(OR.getProperty("search_button"))).click();
+////
+////			JavascriptExecutor jse = (JavascriptExecutor) ob;
+////			jse.executeScript("scroll(0,-500)");
+////			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("profilesTabHeading_link")),30);
+////			ob.findElement(By.xpath(OR.getProperty("profilesTabHeading_link"))).click();
+////			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("search_follow_button")), 40);
+////			ob.findElement(By.xpath(OR.getProperty("search_follow_button"))).click();
+////			Thread.sleep(3000);
+////			logout();
+//
+//			// 3)Verify that User1 receives a notification with correct data
+//			pf.getLoginTRInstance(ob).enterTRCredentials(user1, CONFIG.getProperty("defaultPassword"));
+//			pf.getLoginTRInstance(ob).clickLogin();
+//			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("notification")),30);
+//			String text = ob.findElement(By.xpath(OR.getProperty("notification"))).getText();
+//			System.out.println(text);
+//			
+//			try {
+//				Assert.assertTrue(/*text.contains("TODAY") &&*/ text.contains(fn2 + " " + ln2) && text.contains("is now following you"));
+//				test.log(LogStatus.PASS, "User receiving notification with correct content");
+//			} catch (Throwable t) {
+//
+//				test.log(LogStatus.FAIL, "User receiving notification with incorrect content");// extent
+//																								// reports
+//				test.log(LogStatus.INFO, "Error--->" + t);
+//				ErrorUtil.addVerificationFailure(t);
+//				status = 2;// excel
+//				test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(captureScreenshot(
+//						this.getClass().getSimpleName() + "_user_receiving_notification_with_incorrect_content")));// screenshot
+//
+//			}
+//
+//			closeBrowser();
+//
+//		} catch (Throwable t) {
+//			test.log(LogStatus.FAIL, "Something unexpected happened");// extent
+//																		// reports
+//			// next 3 lines to print whole testng error in report
+//			StringWriter errors = new StringWriter();
+//			t.printStackTrace(new PrintWriter(errors));
+//			test.log(LogStatus.INFO, errors.toString());// extent reports
+//			ErrorUtil.addVerificationFailure(t);// testng
+//			status = 2;// excel
+//			test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(
+//					captureScreenshot(this.getClass().getSimpleName() + "_something_unexpected_happened")));// screenshot
+//			closeBrowser();
+//		}
+//
+//		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution ends--->");
 	}
 
 	@AfterTest
