34dfff19675df64fbea18c188f05b1c9bf6dfe1a
Sachin Bennur <sachin.bennur@thomsonreuters.com>
2016-04-13 17:29:36 +0530

    Committing TestBase

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -31,6 +32,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -217,26 +219,34 @@ public class TestBase {
 	// }
 
 	// Opening via Sauce Labs
-	/*
-	 * public void openBrowser() throws Exception { DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-	 * desiredCapabilities.setBrowserName(System.getenv("SELENIUM_BROWSER"));
-	 * System.out.println("Selenium Browser Name-->" + System.getenv("SELENIUM_BROWSER"));
-	 * desiredCapabilities.setVersion(System.getenv("SELENIUM_VERSION")); System.out.println("Selenium Version-->" +
-	 * System.getenv("SELENIUM_VERSION")); System.out.println("Selenium Plaform-->" +
-	 * System.getenv("SELENIUM_PLATFORM")); desiredCapabilities.setCapability(CapabilityType.PLATFORM,
-	 * System.getenv("SELENIUM_PLATFORM")); desiredCapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true); //
-	 * desiredCapabilities.setCapability(CapabilityType.HAS_NATIVE_EVENTS, true); ob = new RemoteWebDriver( new
-	 * URL("http://amneetsingh:f48a9e78-a431-4779-9592-1b49b6d406a4@ondemand.saucelabs.com:80/wd/hub"),
-	 * desiredCapabilities); String waitTime = CONFIG.getProperty("defaultImplicitWait"); String pageWait =
-	 * CONFIG.getProperty("defaultPageWait"); ob.manage().timeouts().implicitlyWait(Long.parseLong(waitTime),
-	 * TimeUnit.SECONDS); try { ob.manage().timeouts().implicitlyWait(Long.parseLong(pageWait), TimeUnit.SECONDS); }
-	 * catch (Throwable t) { System.out.println("Page Load Timeout not supported in safari driver"); } }
-	 */
+	
+	public void openBrowser() throws Exception {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setBrowserName(System.getenv("SELENIUM_BROWSER"));
+		System.out.println("Selenium Browser Name-->" + System.getenv("SELENIUM_BROWSER"));
+		desiredCapabilities.setVersion(System.getenv("SELENIUM_VERSION"));
+		System.out.println("Selenium Version-->" + System.getenv("SELENIUM_VERSION"));
+		System.out.println("Selenium Plaform-->" + System.getenv("SELENIUM_PLATFORM"));
+		desiredCapabilities.setCapability(CapabilityType.PLATFORM, System.getenv("SELENIUM_PLATFORM"));
+		desiredCapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true); //
+		desiredCapabilities.setCapability(CapabilityType.HAS_NATIVE_EVENTS, true);
+		ob = new RemoteWebDriver(new URL(
+				"http://amneetsingh:f48a9e78-a431-4779-9592-1b49b6d406a4@ondemand.saucelabs.com:80/wd/hub"),
+				desiredCapabilities);
+		String waitTime = CONFIG.getProperty("defaultImplicitWait");
+		String pageWait = CONFIG.getProperty("defaultPageWait");
+		ob.manage().timeouts().implicitlyWait(Long.parseLong(waitTime), TimeUnit.SECONDS);
+		try {
+			ob.manage().timeouts().implicitlyWait(Long.parseLong(pageWait), TimeUnit.SECONDS);
+		} catch (Throwable t) {
+			System.out.println("Page Load Timeout not supported in safari driver");
+		}
+	}
 
 	// selenium RC/ Webdriver
 
 	// Opening the desired browser
-	public void openBrowser() {
+	/*public void openBrowser() {
 
 		if (CONFIG.getProperty("browserType").equals("FF")) {
 			ob = new FirefoxDriver();
@@ -274,7 +284,7 @@ public class TestBase {
 			System.out.println("Page Load Timeout not supported in safari driver");
 		}
 
-	}
+	}*/
 
 	// Closing the browser
 	public void closeBrowser() {
