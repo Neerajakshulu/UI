7e742c692e189e8cc9277dc5690a8a0ce8705c10
Kavya Revanna <kavya.revanna@thomsonreuters.com>
2016-05-04 19:03:51 +0530

    Script Fix for FF

--- src/test/java/Authoring/Authoring.java
+++ src/test/java/Authoring/Authoring.java
@@ -79,8 +79,9 @@ public class Authoring extends TestBase {
 		jse.executeScript("scroll(0,"+ ycord+");");
 		BrowserWaits.waitTime(5);
 		jsClick(ob,commentArea);
-		BrowserWaits.waitTime(3);
-		commentArea.sendKeys(addComments + RandomStringUtils.randomNumeric(3));
+		commentArea.clear();
+		String comment=addComments + RandomStringUtils.randomNumeric(3);
+		commentArea.sendKeys(comment);
 		//new Actions(ob).moveToElement(commentArea).sendKeys(addComments).build().perform();
 		Thread.sleep(5000);// after entering the comments wait for submit button to get enabled or disabled
 	}
