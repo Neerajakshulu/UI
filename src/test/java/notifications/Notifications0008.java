92bea73aed7e68b3c50b1d8ef35b0c77136c02e3
UC201214 <UC201214@UC201214-TPL-A.ten.thomsonreuters.com>
2016-05-30 17:46:40 +0530

    Taking changes

--- src/test/java/notifications/Notifications0008.java
+++ src/test/java/notifications/Notifications0008.java
@@ -1,5 +1,7 @@
 package notifications;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.List;
 
 import org.apache.commons.lang3.RandomStringUtils;
@@ -90,6 +92,25 @@ public class Notifications0008 extends NotificationsTestBase {
 					extent.endTest(test);
 				}
 
+				try {
+					extent = ExtentManager.getReporter(filePath);
+					test = extent
+							.startTest("OPQA-1395",
+									"Verify that all users receive notification when other user published a post and validate notification.")
+							.assignCategory("Notifications");
+					test.log(LogStatus.INFO, "Published a post -" + postString);
+					if (poststatus) {
+						Verifypostnotification();
+					} else
+						throw new Exception("Post creation Exception");
+
+				} catch (Exception e) {
+					test.log(LogStatus.FAIL, e.getMessage());
+					e.printStackTrace();
+				} finally {
+					extent.endTest(test);
+				}
+
 				// Verify that user is receiving notification when someone liked his post(aggregated notification)
 				try {
 					extent = ExtentManager.getReporter(filePath);
@@ -108,6 +129,10 @@ public class Notifications0008 extends NotificationsTestBase {
 				} finally {
 					extent.endTest(test);
 				}
+				boolean commentstatus = false;
+				if (poststatus) {
+					commentstatus = addCommentOnPost();
+				}
 				// Verify that user able to recevie's a notification when other user commented on his post
 				try {
 					extent = ExtentManager.getReporter(filePath);
@@ -116,13 +141,36 @@ public class Notifications0008 extends NotificationsTestBase {
 									"Verify that user able to recevie's a notification when other user commented on his post")
 							.assignCategory("Notifications");
 					test.log(LogStatus.INFO, "Published a post -" + postString);
-					if (poststatus) {
+					if (commentstatus && poststatus) {
 						notification3();
 					} else
 						throw new Exception("Post creation Exception");
 				} catch (Exception e) {
 					test.log(LogStatus.FAIL, e.getMessage());
 					e.printStackTrace();
+				} finally {
+					extent.endTest(test);
+				}
+				try {
+					extent = ExtentManager.getReporter(filePath);
+					test = extent
+							.startTest("OPQA-1397",
+									"Verify that all users receive notification when other user published a comment on post and validate notification.")
+							.assignCategory("Notifications");
+					if (commentstatus && poststatus) {
+						test.log(LogStatus.INFO, "added commnet on post successfully");
+						verifycommentNotification();
+					} else {
+						if (!commentstatus) {
+							test.log(LogStatus.INFO, "Facing issue with adding comment");
+						}
+						throw new Exception(
+								"Failed to recevie a notification when someone he is following user comments on a post");
+					}
+
+				} catch (Throwable t) {
+					test.log(LogStatus.FAIL, t.getMessage());
+					logger.info(t.getMessage());
 				} finally {
 					extent.endTest(test);
 				}
@@ -159,9 +207,65 @@ public class Notifications0008 extends NotificationsTestBase {
 
 	}
 
-	private void notification3() throws Exception {
+	private void verifycommentNotification() throws Exception {
 		try {
-			// USER1 WILL COMMENT ON THE POST CREATED
+			pf.getLoginTRInstance(ob).enterTRCredentials(CONFIG.getProperty("defaultUsername"),
+					CONFIG.getProperty("defaultPassword"));
+			pf.getLoginTRInstance(ob).clickLogin();
+			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("searchBox_textBox")), 30);
+			BrowserWaits.waitTime(10);
+			JavascriptExecutor jse = (JavascriptExecutor) ob;
+			List<WebElement> listOfNotifications = null;
+			String text = null;
+			for (int i = 1; i <= 3; i++) {
+
+				BrowserWaits.waitTime(3);
+				listOfNotifications = ob.findElements(By.xpath(OR.getProperty("all_notifications_in_homepage")));
+				for (int j = 0; j < listOfNotifications.size(); j++) {
+					String temp = listOfNotifications.get(j).getText();
+					if (temp.contains(fn1 + " " + ln1) && temp.contains("New Comment") && temp.contains(postString)) {
+						text = temp;
+					}
+				}
+				if (text.length() > 0) {
+					break;
+				}
+				jse.executeScript("window.scrollTo(0, document.body.scrollHeight)", "");
+			}
+			logger.info("Notification Text: " + text);
+			try {
+				Assert.assertTrue(text.contains("New Comment") && text.contains(fn1 + " " + ln1)
+						&& text.contains("commented on") && text.contains(postString));
+				test.log(LogStatus.PASS, "User receiving notification with correct content");
+			} catch (Throwable t) {
+				test.log(LogStatus.FAIL, "User receiving notification with incorrect content");// extent
+				StringWriter errors = new StringWriter();
+				t.printStackTrace(new PrintWriter(errors));
+				test.log(LogStatus.INFO, errors.toString()); // reports
+				test.log(LogStatus.INFO, "Error--->" + t.getMessage());
+				ErrorUtil.addVerificationFailure(t);
+				status = 2;// excel
+				test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(captureScreenshot(
+						this.getClass().getSimpleName() + "_user_receiving_notification_with_incorrect_content")));// screenshot
+			}
+		} catch (Throwable t) {
+			test.log(LogStatus.FAIL, "Something unexpected happened" + t);// extent
+			// next 3 lines to print whole testng error in report
+			StringWriter errors = new StringWriter();
+			t.printStackTrace(new PrintWriter(errors));
+			test.log(LogStatus.INFO, errors.toString());// extent reports
+			ErrorUtil.addVerificationFailure(t);// testng
+			status = 2;// excel
+			test.log(LogStatus.INFO, "Snapshot below: "
+					+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName() + screen++)));// screenshot
+		} finally {
+			pf.getLoginTRInstance(ob).logOutApp();
+		}
+	}
+
+	private boolean addCommentOnPost() throws Exception {
+		boolean status = false;
+		try {
 			pf.getLoginTRInstance(ob).enterTRCredentials(user1, CONFIG.getProperty("defaultPassword"));
 			pf.getLoginTRInstance(ob).clickLogin();
 			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("searchBox_textBox")), 30);
@@ -176,13 +280,25 @@ public class Notifications0008 extends NotificationsTestBase {
 			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("search_results_post_link")), 30);
 			ob.findElement(By.xpath(OR.getProperty("search_results_post_link"))).click();
 			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("document_comment_textbox")), 30);
-			ob.findElement(By.xpath(OR.getProperty("document_comment_textbox"))).sendKeys("Very Nice Post");
+			ob.findElement(By.xpath(OR.getProperty("document_comment_textbox")))
+					.sendKeys(OR.getProperty("COMMENT_TEXT"));
 			BrowserWaits.waitTime(5);
 			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("document_addComment_button")), 30);
 			jsClick(ob, ob.findElement(By.xpath(OR.getProperty("document_addComment_button"))));
 			BrowserWaits.waitTime(6);
+			status = true;
+		} catch (Exception e) {
+			logger.error("Probem happens while adding comment on post" + e.getMessage());
+			captureScreenshot(this.getClass().getSimpleName() + "_adding_cooment");
+			// throw new Exception("Probem happens while adding comment on post" + e.getMessage());
+		} finally {
 			pf.getLoginTRInstance(ob).logOutApp();
+		}
+		return status;
+	}
 
+	private void notification3() throws Exception {
+		try {
 			// LOGIN WITH USER3 AND CHECK FOR THE NOTIFICATION
 			pf.getLoginTRInstance(ob).enterTRCredentials(user2, CONFIG.getProperty("defaultPassword"));
 			pf.getLoginTRInstance(ob).clickLogin();
@@ -193,7 +309,7 @@ public class Notifications0008 extends NotificationsTestBase {
 				Assert.assertTrue(text.contains("New comments on your post") && /*
 																				 * text.contains("TODAY") &&
 																				 */text.contains(postString)
-						&& text.contains(fn1 + " " + ln1) && text.contains("Very Nice Post"));
+						&& text.contains(fn1 + " " + ln1) && text.contains(OR.getProperty("COMMENT_TEXT")));
 				test.log(LogStatus.PASS, "User receiving notification with correct content");
 			} catch (Throwable t) {
 
@@ -299,6 +415,50 @@ public class Notifications0008 extends NotificationsTestBase {
 			try {
 				Assert.assertTrue(/* text.contains("TODAY") && */text.contains(expected_text)
 						&& text.contains("published a post") && text.contains(postString));
+				test.log(LogStatus.PASS, "User receiving notification with correct content");
+			} catch (Throwable t) {
+				test.log(LogStatus.FAIL, "User receiving notification with incorrect content");// extent
+				// reports
+				test.log(LogStatus.INFO, "Error--->" + t.getMessage());
+				ErrorUtil.addVerificationFailure(t);
+				status = 2;// excel
+				test.log(LogStatus.INFO, "Snapshot below: "
+						+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName() + screen++)));// screenshot
+			}
+		} catch (Exception e) {
+			test.log(LogStatus.FAIL, "Fail");
+			logger.error(e.getMessage());
+			e.printStackTrace();
+		} finally {
+			pf.getLoginTRInstance(ob).logOutApp();
+		}
+	}
+
+	private void Verifypostnotification() throws Exception {
+		try {
+			// Login using user1 and check for the notification
+			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("TR_login_button")), 20);
+			pf.getLoginTRInstance(ob).enterTRCredentials(CONFIG.getProperty("defaultUsername"),
+					CONFIG.getProperty("defaultPassword"));
+			pf.getLoginTRInstance(ob).clickLogin();
+			BrowserWaits.waitTime(5);
+			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("header_label")), 50);
+			// String text = ob.findElement(By.xpath(OR.getProperty("notificationForNewPost"))).getText();
+			String text = null;
+			List<WebElement> listOfNotifications = ob
+					.findElements(By.xpath(OR.getProperty("all_notifications_in_homepage")));
+			for (int i = 0; i < listOfNotifications.size(); i++) {
+				text = listOfNotifications.get(i).getText();
+				if (text.contains("published a post") && text.contains(postString)) {
+					break;
+				}
+			}
+			logger.info("Notification Text: " + text);
+			String expected_text = fn2 + " " + ln2;
+			try {
+				Assert.assertTrue(
+						/* text.contains("TODAY") && */text.contains("New Post") && text.contains(expected_text)
+								&& text.contains("published a post") && text.contains(postString));
 				test.log(LogStatus.PASS, "User receiving notification with correct content");
 			} catch (Throwable t) {
 				test.log(LogStatus.FAIL, "User receiving notification with incorrect content");// extent
