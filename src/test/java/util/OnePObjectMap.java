ef6cba088ccbff5a2e9e6cc52a892e2aa14acd92
UC204155 <UC204155@UC204155-TPD-A.ten.thomsonreuters.com>
2016-06-21 17:06:18 +0530

    Fixed failing scripts in IAM and Notification Module.

--- src/test/java/util/OnePObjectMap.java
+++ src/test/java/util/OnePObjectMap.java
@@ -243,14 +243,17 @@ public enum OnePObjectMap {
 	SEARCH_RESULTS_PAGE_PEOPLE_TITLE_CSS("div[class='wui-card__content'] a[class='ng-binding ng-scope']"),
 	SEARCH_RESULTS_PAGE_POST_AUTHOR_CSS("a[event-category='searchresult-ck-profile'][class='ng-binding ng-scope']"),
 	WTCHLISH_LINK_XPATH("//span[contains(text(),'Watchlist')]"),
-	NEWSFEED_RECOMMENDED_ARTICLES_SECTION_XPATH("(//div[@class='wui-card wui-card--recommended-articles']/div)[2]/div"), 
+	NEWSFEED_RECOMMENDED_ARTICLES_SECTION_ARTICLE_XPATH("//div[@class='wui-card__line-item ng-scope']"), 
 	NEWSFEED_RECOMMENDED_ARTICLE_TITLE_XPATH("//div[@class='wui-content-title ng-binding wui-content-title--small']"),
 	ARTICLE_TITLE_IN_RECORD_VIEW_PAGE_CSS("div[class='ne-publication__header'] h2"),
 	NEWSFEED_RECOMMENDED_PEOPLE_SECTION_FOLLOW_USER_CSS("div[class='wui-card ng-scope'] span[class='fa fa-check unfollow']"),
 	NEWSFEED_RECOMMENDED_PEOPLE_SECTION_NUMBER_OF_USER_CSS("div[class='col-xs-12 images image-wrapper ng-scope"),
 	NEWSFEED_RECOMMENDED_PEOPLE_SECTION_COPY_USER_NAME_CSS("span[class='ne-profile-object-title']"),
-	
-	
+	NEWSFEED_RECOMMENDED_ARTICLES_SECTION_XPATH("//div[@class='wui-card wui-card--recommended-articles']"),
+	NEWSFEED_RECOMMENDED_ARTICLES_SECTION_ARTICLE_NAME_XPATH("//div[@class='wui-content-title ng-binding wui-content-title--small']"),
+	NEWSFEED_RECOMMENDED_ARTICLES_SECTION_WATCHLIST_BUTTON_XPATH("//div[@class='wui-card wui-card--recommended-articles']//button"),
+	NEWSFEED_RECOMMENDED_PEOPLE_SECTION_XPATH("//div[@class='wui-card ng-scope']"),
+	PROFILE_PAGE_AUTOR_NAME_CSS("h2[class='wui-title wui-title--user-profile-name ng-binding']"),
 	;
 
 	
