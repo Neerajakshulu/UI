4a55f12ed7fdccdd5af442a8ad9e7da5b5d0f9b7
Chinna Putha <chinna.putha@thomsonreuters.com>
2016-09-13 12:38:26 +0530

    profile onboarding script implementation

--- src/test/java/pages/OnboardingModalsPage.java
+++ src/test/java/pages/OnboardingModalsPage.java
@@ -1,7 +1,11 @@
 package pages;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
@@ -237,21 +241,8 @@ public class OnboardingModalsPage extends TestBase {
 		logger.info("onboarding status-->" + onboardingStatus.size());
 
 		try {
-			pf.getBrowserWaitsInstance(ob)
-					.waitUntilElementIsClickable(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_WELCOME_MODAL_CSS);
-			pf.getBrowserWaitsInstance(ob).waitUntilText("Follow and discuss research", "Connect with researchers",
-					"Discover articles, patents, and community contributions");
-			pf.getBrowserWaitsInstance(ob).waitUntilText("Recommended people to follow");
-			pf.getBrowserActionInstance(ob).click(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_WELCOME_MODAL_CSS);
-			pf.getBrowserWaitsInstance(ob).waitUntilElementIsClickable(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_CSS);
+			profileOnboardingModal();
 			
-			pf.getBrowserWaitsInstance(ob).waitUntilElementIsDisplayed(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_IMAGE_UPLOAD_CSS);
-			pf.getBrowserWaitsInstance(ob).waitUntilElementIsDisplayed(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_IMAGE_DELETE_CSS);
-			pf.getBrowserWaitsInstance(ob).waitUntilElementIsClickable(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_IMAGE_EDIT_CSS);
-			
-			pf.getBrowserWaitsInstance(ob).waitUntilText("Let’s learn about you","Interests and Skills","Done");
-			pf.getBrowserWaitsInstance(ob).waitUntilText("Add a Topic","You can always complete your profile later from  your Profile page.");
-			pf.getBrowserWaitsInstance(ob).waitUntilText("Note:","Information you add to your profile will be seen by others.");
 			pf.getBrowserActionInstance(ob).click(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_CSS);
 			BrowserWaits.waitTime(2);
 			
@@ -266,6 +257,107 @@ public class OnboardingModalsPage extends TestBase {
 			throw new Exception("Profile Onboarding Modals are not displayed for First time user");
 		}
 
+	}
+	
+	
+	/**
+	 * Method for Validate profile name profile onboarding modal ,
+	 * @throws Exception, When profile name is not updated 
+	 */
+	public void updateNameOnProfileOnboardingModal(String profileFieldsDefaultText) throws Exception {
+
+		List<WebElement> onboardingStatus = pf.getBrowserActionInstance(ob)
+				.getElements(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_MODAL_CSS);
+		logger.info("onboarding status-->" + onboardingStatus.size());
+		List<String> defaultText= new ArrayList<String>();
+		String placeholders[]=profileFieldsDefaultText.split("\\|");
+		List<String> profileFieldsPlaceholders=Arrays.asList(placeholders);
+		
+		try {
+			profileOnboardingModal();
+			
+			pf.getBrowserActionInstance(ob).clickAndClear(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_FIRST_NAME_CSS);
+			pf.getBrowserActionInstance(ob).clickAndClear(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_LAST_NAME_CSS);
+			pf.getBrowserActionInstance(ob).clickAndClear(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_ROLE_CSS);
+			pf.getBrowserActionInstance(ob).clickAndClear(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_PI_CSS);
+			pf.getBrowserActionInstance(ob).clickAndClear(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_COUNTRY_CSS);
+			
+			defaultText.add(pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_FIRST_NAME_CSS).getAttribute("placeholder"));
+			defaultText.add(pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_LAST_NAME_CSS).getAttribute("placeholder"));
+			defaultText.add(pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_ROLE_CSS).getAttribute("placeholder"));
+			defaultText.add(pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_PI_CSS).getAttribute("placeholder"));
+			defaultText.add(pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_COUNTRY_CSS).getAttribute("placeholder"));
+			
+			logger.info("placeholder input text-->"+profileFieldsPlaceholders);
+			logger.info("placeholder app text-->"+defaultText);
+			if(!(profileFieldsPlaceholders.equals(defaultText))) {
+				throw new Exception("Profile info/metadata fields doesn't have any placeholder values");
+			}
+			
+			String firstName=RandomStringUtils.randomAlphabetic(10);
+			String lastName=RandomStringUtils.randomAlphabetic(20);
+			
+			pf.getBrowserActionInstance(ob).enterFieldValue(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_FIRST_NAME_CSS, firstName);
+			pf.getBrowserActionInstance(ob).enterFieldValue(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_LAST_NAME_CSS,lastName);
+			
+			pf.getBrowserActionInstance(ob).click(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_CSS);
+			BrowserWaits.waitTime(2);
+			
+			List<WebElement> onboarding_modals=pf.getBrowserActionInstance(ob).getElements(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_CSS);
+			logger.info("onboarding_modals size-->"+onboarding_modals.size());
+			if(!(onboarding_modals.size()==0)) {
+				throw new Exception("Onboarding Modals are not closed");
+			}
+			pf.getBrowserWaitsInstance(ob).waitUntilElementIsClickable(OnePObjectMap.HOME_PROJECT_NEON_SEARCH_BOX_CSS);
+			pf.getBrowserWaitsInstance(ob).waitUntilText("Trending on Neon", "Posts","Articles","Topics","New post","Recommended");
+			
+			pf.getHFPageInstance(ob).clickOnProfileLink();
+			
+			boolean isProfileEditable=pf.getProfilePageInstance(ob).isProfileIncomplete();
+			logger.info("is Profile editable-->"+isProfileEditable);
+			
+			if(isProfileEditable) {
+				pf.getBrowserActionInstance(ob).click(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_EDIT_UPDATE_CSS);
+				BrowserWaits.waitTime(3);
+			}
+			String title = pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_TITLE_CSS).getText();
+			logger.info("Profile title-->"+title);
+			if(!(title.contains(firstName) && title.contains(lastName))) {
+				throw new Exception("profile onboarding first name and last name is not displayed in profile page");
+			}
+			
+		} catch (Exception e) {
+			throw new Exception("Profile Onboarding Modals are not displayed for First time user");
+		}
+
+	}
+	
+	
+	/**
+	 * Method for Validate profile onboarding modal ,
+	 * @throws Exception, When profile onboarding modal not having profile info
+	 */
+	public void profileOnboardingModal() throws Exception {
+
+		List<WebElement> onboardingStatus = pf.getBrowserActionInstance(ob)
+				.getElements(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_MODAL_CSS);
+		logger.info("onboarding status-->" + onboardingStatus.size());
+
+			pf.getBrowserWaitsInstance(ob)
+					.waitUntilElementIsClickable(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_WELCOME_MODAL_CSS);
+			pf.getBrowserWaitsInstance(ob).waitUntilText("Follow and discuss research", "Connect with researchers",
+					"Discover articles, patents, and community contributions");
+			pf.getBrowserWaitsInstance(ob).waitUntilText("Recommended people to follow","OK");
+			pf.getBrowserActionInstance(ob).click(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_WELCOME_MODAL_CSS);
+			pf.getBrowserWaitsInstance(ob).waitUntilElementIsClickable(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_CSS);
+			
+			pf.getBrowserWaitsInstance(ob).waitUntilElementIsDisplayed(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_IMAGE_UPLOAD_CSS);
+			pf.getBrowserWaitsInstance(ob).waitUntilElementIsDisplayed(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_IMAGE_DELETE_CSS);
+			pf.getBrowserWaitsInstance(ob).waitUntilElementIsClickable(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_IMAGE_EDIT_CSS);
+			
+			pf.getBrowserWaitsInstance(ob).waitUntilText("Let’s learn about you","Interests and Skills","Done");
+			pf.getBrowserWaitsInstance(ob).waitUntilText("Add a Topic","You can always complete your profile later from  your Profile page.");
+			pf.getBrowserWaitsInstance(ob).waitUntilText("Note:","Information you add to your profile will be seen by others.");
 	}
 
 }
