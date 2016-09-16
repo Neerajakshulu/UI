5e4470185a2c051920e6c1f5647d65c4283095ba
Bhavyasree Gogineni <bhavyasree.gogineni@thomsonreuters.com>
2016-09-16 11:20:06 +0530

    Add to ENWIAM

---
+++ src/test/java/enwiam/ENWIAM0001.java
@@ -0,0 +1,107 @@
+package enwiam;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import base.TestBase;
+import pages.PageFactory;
+import util.ErrorUtil;
+import util.ExtentManager;
+import util.TestUtil;
+
+
+public class ENWIAM0001 extends TestBase {
+	// Following is the list of status:
+	// 1--->PASS
+	// 2--->FAIL
+	// 3--->SKIP
+	// Checking whether this test case should be skipped or not
+        PageFactory pf=new PageFactory();
+        
+       
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		extent = ExtentManager.getReporter(filePath);
+		rowData = testcase.get(this.getClass().getSimpleName());
+		test = extent.startTest(rowData.getTestcaseId(), rowData.getTestcaseDescription()).assignCategory("ENWIAM");
+
+	}
+
+	@Test
+	public void testLogin() throws Exception {
+		
+		boolean testRunmode = TestUtil.isTestCaseRunnable(enwiamxls, this.getClass().getSimpleName());
+		boolean master_condition = suiteRunmode && testRunmode;
+
+		if (!master_condition) {
+
+			test.log(LogStatus.SKIP,
+					"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
+			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
+
+		}
+
+		
+		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts--->");
+		try {
+			
+			openBrowser();
+			maximizeWindow();
+			clearCookies();
+			String userName="arvindkandaswamy@gmail.com";
+			String password="Kanda@123";
+			
+			ob.get(host+CONFIG.getProperty("appendENWAppUrl"));
+			//Need to add script code
+			
+			
+			pf.getLoginTRInstance(ob).enterTRCredentials(userName, password);
+			pf.getLoginTRInstance(ob).clickLogin();
+			
+			ob.findElement(By.className("btn-common")).click();
+			
+			
+			
+		
+			
+			
+			
+		} catch (Throwable t) {
+			test.log(LogStatus.FAIL, "Something unexpected happened");// extent
+																		// reports
+			// next 3 lines to print whole testng error in report
+			StringWriter errors = new StringWriter();
+			t.printStackTrace(new PrintWriter(errors));
+			test.log(LogStatus.INFO, errors.toString());// extent reports
+			ErrorUtil.addVerificationFailure(t);// testng
+			test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(
+					captureScreenshot(this.getClass().getSimpleName() + "_something_unexpected_happened")));// screenshot
+			closeBrowser();
+		}
+
+		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution ends--->");
+	}
+
+	@AfterTest
+	public void reportTestResult() {
+		extent.endTest(test);
+
+		/*
+		 * if(status==1) TestUtil.reportDataSetResult(iamxls, "Test Cases",
+		 * TestUtil.getRowNum(iamxls,this.getClass().getSimpleName()), "PASS");
+		 * else if(status==2) TestUtil.reportDataSetResult(iamxls, "Test Cases",
+		 * TestUtil.getRowNum(iamxls,this.getClass().getSimpleName()), "FAIL");
+		 * else TestUtil.reportDataSetResult(iamxls, "Test Cases",
+		 * TestUtil.getRowNum(iamxls,this.getClass().getSimpleName()), "SKIP");
+		 */
+	}
+}
