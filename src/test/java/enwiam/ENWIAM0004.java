8c92251c403d3eb7a7c6bbe1d545bd34402fd451
SB94731 <SB94731@PB019WKG.TechMahindra.com>
2016-09-17 18:04:05 +0530

    Arvind STeAM linked Social account with one ENWReference method

---
+++ src/test/java/enwiam/ENWIAM0004.java
@@ -0,0 +1,132 @@
+package enwiam;
+
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import Authoring.LoginTR;
+import base.TestBase;
+import util.BrowserWaits;
+import util.ErrorUtil;
+import util.ExtentManager;
+import util.OnePObjectMap;
+import util.TestUtil;
+
+
+
+/*
+ * Checking linked STeAM with LinkedIn account on ENW
+ */
+public class ENWIAM0004 extends TestBase {
+	static int status = 1;
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		extent = ExtentManager.getReporter(filePath);
+		String var = xlRead2(returnExcelPath('A'), this.getClass().getSimpleName(), 1);
+		test = extent
+				.startTest(var, "Verify that deep linking is working correctly for help page using FB and LI accounts")
+				.assignCategory("IAM");
+
+	}
+
+	@Test
+	public void testcase0004() throws Exception {
+
+		boolean suiteRunmode = TestUtil.isSuiteRunnable(suiteXls, "IAM");
+		boolean testRunmode = TestUtil.isTestCaseRunnable(iamxls, this.getClass().getSimpleName());
+		boolean master_condition = suiteRunmode && testRunmode;
+
+		if (!master_condition) {
+
+			status = 3;
+			test.log(LogStatus.SKIP,
+					"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
+			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
+
+		}
+		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts--->");
+
+		try {
+			//String statuCode = deleteUserAccounts(LOGIN.getProperty("USERNAME17"));
+			//Assert.assertTrue(statuCode.equalsIgnoreCase("200"));
+			loginTofb();
+			//linkAccounts("Facebook");
+		} catch (Throwable t) {
+			test.log(LogStatus.FAIL, "Delete accounts api call failed");// extent
+			ErrorUtil.addVerificationFailure(t);
+		}
+		
+		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution ends--->");
+	}
+
+	private void loginTofb() throws Exception {
+		openBrowser();
+		maximizeWindow();
+		clearCookies();
+         String userName="arvindkandaswamy@gmail.com";
+         String passWord="darshiniyogi";
+        		 String accountType="LinkedIn";
+		// Navigate to TR login page and login with valid TR credentials
+		//ob.navigate().to(host);
+        		 ob.get(host+CONFIG.getProperty("appendENWAppUrl"));
+		pf.getENWReferencePageInstance(ob).loginWithENWLinkedInCredentials(userName,passWord);
+		//(LOGIN.getProperty("USERNAME17"),	LOGIN.getProperty("PASSWORD17"));
+		pf.getENWReferencePageInstance(ob).clickAccount();
+		pf.getENWReferencePageInstance(ob).closeOnBoardingModal();
+		validateLinkedAccounts(2,accountType);
+		pf.getENWReferencePageInstance(ob).logout1();
+		closeBrowser();
+		pf.clearAllPageObjects();
+
+	}
+
+	//Validating the linked Account
+			private void validateLinkedAccounts(int accountCount, String linkName) throws Exception {
+				try {
+
+					Assert.assertTrue(
+							pf.getAccountPageInstance(ob).verifyLinkedAccount("Neon", LOGIN.getProperty("UserName40")));
+					Assert.assertTrue(
+							pf.getAccountPageInstance(ob).verifyLinkedAccount(linkName, LOGIN.getProperty("UserName40")));
+					test.log(LogStatus.PASS , "The account are matching");
+					Assert.assertTrue(pf.getAccountPageInstance(ob).validateAccountsCount(accountCount));
+					System.out.println(accountCount);
+					test.log(LogStatus.PASS,
+							"Linked accounts are available in accounts page : Neon and " + linkName + " accounts");
+
+				} catch (Throwable t) {
+					test.log(LogStatus.FAIL,
+							"Linked accounts are not available in accounts page : Neon and " + linkName + " accounts");
+					ErrorUtil.addVerificationFailure(t);// testng
+					test.log(LogStatus.INFO, "Snapshot below: "
+							+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName() + "Linking_failed")));// screenshot
+				}
+			}
+	
+	@AfterTest
+	public void reportTestResult() {
+		extent.endTest(test);
+
+		/*
+		 * if(status==1) TestUtil.reportDataSetResult(iamxls, "Test Cases",
+		 * TestUtil.getRowNum(iamxls,this.getClass().getSimpleName()), "PASS");
+		 * else if(status==2) TestUtil.reportDataSetResult(iamxls, "Test Cases",
+		 * TestUtil.getRowNum(iamxls,this.getClass().getSimpleName()), "FAIL");
+		 * else TestUtil.reportDataSetResult(iamxls, "Test Cases",
+		 * TestUtil.getRowNum(iamxls,this.getClass().getSimpleName()), "SKIP");
+		 */
+	}
+
+}
--- src/test/java/pages/ENWReferencePage.java
+++ src/test/java/pages/ENWReferencePage.java
@@ -5,8 +5,14 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import com.relevantcodes.extentreports.LogStatus;
 
 import base.TestBase;
+import util.BrowserWaits;
+import util.ErrorUtil;
+import util.OnePObjectMap;
 
 /**
  * This class contains all the method related to account page
@@ -39,6 +45,108 @@ public class ENWReferencePage extends TestBase {
 		ob.findElement(By.cssSelector("input[id='idDeleteTrash']")).click();
 		
 		
+	}
+	
+	
+	//ENW FB sign in 
+	public void loginWithENWFBCredentials(String username, String pwd) throws InterruptedException, Exception {
+
+		waitForElementTobeVisible(ob, By.cssSelector(OnePObjectMap.LOGIN_PAGE_FB_SIGN_IN_BUTTON_CSS.toString()), 30);
+		ob.findElement(By.cssSelector(OnePObjectMap.LOGIN_PAGE_FB_SIGN_IN_BUTTON_CSS.toString())).click();
+
+		signInToFacebook(username, pwd);
+		closeOnBoardingModal();
+	}
+	public void signInToFacebook(String username, String pwd) throws InterruptedException {
+		waitForElementTobeVisible(ob, By.name(OnePObjectMap.LOGIN_PAGE_FB_EMAIL_TEXT_BOX_ID.toString()), 30);
+
+		// Verify that existing LI user credentials are working fine
+		ob.findElement(By.name(OnePObjectMap.LOGIN_PAGE_FB_EMAIL_TEXT_BOX_ID.toString())).sendKeys(username);
+		ob.findElement(By.name(OnePObjectMap.LOGIN_PAGE_FB_PASSWORD_TEXT_BOX_ID.toString())).sendKeys(pwd);
+		BrowserWaits.waitTime(2);
+		ob.findElement(By.xpath(OR.getProperty("ENW_FB_LOGIN_BUTTON"))).click();
+		BrowserWaits.waitTime(2);
+		ob.findElement(By.xpath(OR.getProperty("ENW_CONTINUE_BUTTON"))).click();
+	}
+	
+		public void clickAccount() throws InterruptedException
+		{
+			try {
+				BrowserWaits.waitTime(3);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			ob.findElement(By.xpath(OR.getProperty("ENW_FB_PROFILE_IMGCIRCLE"))).click();
+			BrowserWaits.waitTime(3);
+			ob.findElement(By.xpath(OR.getProperty("ENW_FB_PROFILE_IMGCIRCLE_ACCOUNT"))).click();
+			BrowserWaits.waitTime(3);
+			//validateLinkedAccounts(2, accountType);
+		}
+		
+		
+		// ENW LinkedIn Login
+		public void loginWithENWLinkedInCredentials(String username, String pwd) throws InterruptedException, Exception {
+
+			waitForElementTobeVisible(ob, By.cssSelector(OnePObjectMap.LOGIN_PAGE_LI_SIGN_IN_BUTTON_CSS.toString()), 30);
+			ob.findElement(By.cssSelector(OnePObjectMap.LOGIN_PAGE_LI_SIGN_IN_BUTTON_CSS.toString())).click();
+
+			signInToENWLinkedIn(username, pwd);
+			closeOnBoardingModal();
+		}
+
+		public void signInToENWLinkedIn(String username, String pwd) throws InterruptedException {
+			waitForElementTobeVisible(ob, By.name(OnePObjectMap.LOGIN_PAGE_LI_EMAIL_TEXT_BOX_ID.toString()), 30);
+
+			// Verify that existing LI user credentials are working fine
+			ob.findElement(By.name(OnePObjectMap.LOGIN_PAGE_LI_EMAIL_TEXT_BOX_ID.toString())).sendKeys(username);
+			ob.findElement(By.name(OnePObjectMap.LOGIN_PAGE_LI_PASSWORD_TEXT_BOX_ID.toString())).sendKeys(pwd);
+			// BrowserWaits.waitTime(2);
+			ob.findElement(By.name(OnePObjectMap.LOGIN_PAGE_LI_ALLOW_ACCESS_BUTTON_ID.toString())).click();
+			BrowserWaits.waitTime(2);
+			ob.findElement(By.xpath(OR.getProperty("ENW_CONTINUE_BUTTON"))).click();
+		}
+		
+		
+		// ENW Back to Endnot button +  Logout
+		public void logout1() throws InterruptedException
+		{
+			BrowserWaits.waitTime(3);
+			ob.findElement(By.xpath(OR.getProperty("ENW_BACK_TO_ENDNOTE_BUTTON"))).click();
+			try {
+				closeOnBoardingModal();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			BrowserWaits.waitTime(3);
+			ob.findElement(By.xpath(OR.getProperty("ENW_FB_PROFILE_IMGCIRCLE"))).click();
+			BrowserWaits.waitTime(3);
+			ob.findElement(By.xpath(OR.getProperty("ENW_FB_PROFILE_FLYOUT_SIGNOUT"))).click();
+		}
+		
+	//Closing the onboarding modal 
+	public void closeOnBoardingModal() throws Exception, InterruptedException {
+		List<WebElement> onboardingStatus = pf.getBrowserActionInstance(ob)
+				.getElements(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_MODAL_CSS);
+		logger.info("onboarding status-->" + onboardingStatus.size());
+
+		if (onboardingStatus.size() == 1) {
+
+			String header = pf.getBrowserActionInstance(ob)
+					.getElement(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_WELCOME_MODAL_TEXT_CSS).getText();
+			if (header.contains("Hello")) {
+				pf.getBrowserWaitsInstance(ob)
+						.waitUntilElementIsClickable(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_WELCOME_MODAL_CSS);
+				pf.getBrowserActionInstance(ob).click(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_WELCOME_MODAL_CSS);
+				pf.getBrowserWaitsInstance(ob)
+						.waitUntilElementIsClickable(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_CSS);
+				pf.getBrowserActionInstance(ob).click(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_CSS);
+				BrowserWaits.waitTime(4);
+				pf.getBrowserWaitsInstance(ob)
+						.waitUntilElementIsClickable(OnePObjectMap.HOME_PROJECT_NEON_SEARCH_BOX_CSS);
+			}
+		}
 	}
 
 }
