6157a041144be6362ac288cdc0076a8b386a5a9c
Chinna Putha <chinna.putha@thomsonreuters.com>
2016-09-29 15:24:25 +0530

    profile scripts

--- src/test/java/pages/SearchProfile.java
+++ src/test/java/pages/SearchProfile.java
@@ -28,7 +28,7 @@ public class SearchProfile extends TestBase {
 	public void enterSearchKeyAndClick(String searchKey) throws Exception {
 		pf.getBrowserActionInstance(ob).enterFieldValue(OnePObjectMap.HOME_PROJECT_NEON_SEARCH_BOX_CSS, searchKey);
 		BrowserWaits.waitTime(2);
-		pf.getBrowserActionInstance(ob).click(OnePObjectMap.HOME_PROJECT_NEON_SEARCH_CLICK_CSS);
+		pf.getBrowserActionInstance(ob).click(OnePObjectMap.HOME_PROJECT_NEON_SEARCH_CLICK_CSS); 
 		waitForAjax(ob);
 		pf.getBrowserWaitsInstance(ob).waitUntilElementIsNotDisplayed(OnePObjectMap.NEON_TO_ENW_BACKTOENDNOTE_PAGELOAD_CSS);
 		waitForElementTobeVisible(ob, By.xpath(OR.getProperty("searchResults_links")), 90);
