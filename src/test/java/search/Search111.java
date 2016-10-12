97fe8fc982b3f7365d1da01b84a4f5563cd034dd
bhavyasree-gogineni <bhavyasree.goginnei@thomsonreuters.com>
2016-10-12 13:30:09 +0530

    Modified scripts in search module

--- src/test/java/search/Search111.java
+++ src/test/java/search/Search111.java
@@ -33,8 +33,8 @@ public class Search111 extends TestBase {
 	public void beforeTest() throws Exception {
 		extent = ExtentManager.getReporter(filePath);
 		rowData = testcase.get(this.getClass().getSimpleName());
-		test = extent.startTest(rowData.getTestcaseId(), rowData.getTestcaseDescription())
-				.assignCategory("Search suite");
+		test = extent.startTest(rowData.getTestcaseId(), rowData.getTestcaseDescription()).assignCategory(
+				"Search suite");
 	}
 
 	@Test
@@ -46,8 +46,8 @@ public class Search111 extends TestBase {
 		if (!master_condition) {
 
 			status = 3;// excel
-			test.log(LogStatus.SKIP,
-					"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
+			test.log(LogStatus.SKIP, "Skipping test case " + this.getClass().getSimpleName()
+					+ " as the run mode is set to NO");
 			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
 
 		}
@@ -71,17 +71,17 @@ public class Search111 extends TestBase {
 			pf.getSearchResultsPageInstance(ob).clickOnPeopleTab();
 
 			// checking for Default sort option
-			List<WebElement> beforeScrollSize = ob
-					.findElements(By.xpath(OR.getProperty("tr_search_people_profilename_link_xpath")));
-			BrowserWaits.waitTime(4);
+			List<WebElement> beforeScrollSize = ob.findElements(By.xpath(OR
+					.getProperty("tr_search_people_profilename_link_xpath")));
+			BrowserWaits.waitTime(5);
 			System.out.println(beforeScrollSize.size());
 
 			JavascriptExecutor jse = (JavascriptExecutor) ob;
 			jse.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 			BrowserWaits.waitTime(5);
 
-			List<WebElement> afterScrollSize = ob
-					.findElements(By.xpath(OR.getProperty("tr_search_people_profilename_link_xpath")));
+			List<WebElement> afterScrollSize = ob.findElements(By.xpath(OR
+					.getProperty("tr_search_people_profilename_link_xpath")));
 			System.out.println(afterScrollSize.size());
 
 			try {
@@ -96,8 +96,11 @@ public class Search111 extends TestBase {
 				test.log(LogStatus.INFO, errors.toString());// extent reports
 				ErrorUtil.addVerificationFailure(t);// testng
 				status = 2;// excel
-				test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(
-						captureScreenshot(this.getClass().getSimpleName() + "_something_unexpected_happened")));// screenshot
+				test.log(
+						LogStatus.INFO,
+						"Snapshot below: "
+								+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName()
+										+ "_something_unexpected_happened")));// screenshot
 				closeBrowser();
 			}
 
@@ -114,8 +117,11 @@ public class Search111 extends TestBase {
 			test.log(LogStatus.INFO, errors.toString());// extent reports
 			ErrorUtil.addVerificationFailure(t);// testng
 			status = 2;// excel
-			test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(
-					captureScreenshot(this.getClass().getSimpleName() + "_something_unexpected_happened")));// screenshot
+			test.log(
+					LogStatus.INFO,
+					"Snapshot below: "
+							+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName()
+									+ "_something_unexpected_happened")));// screenshot
 			closeBrowser();
 		}
 
