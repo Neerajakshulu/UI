766739fcf10ba11e3108479eb18c135c7e39f888
SL00437971 <SL00437971@INBASDLP00088.TechMahindra.com>
2016-10-27 20:44:51 +0530

    ENW script implementation

---
+++ src/test/java/enwiam/ENWIAM90.java
@@ -0,0 +1,191 @@
+package enwiam;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import base.TestBase;
+import util.BrowserWaits;
+import util.ErrorUtil;
+import util.ExtentManager;
+import util.OnePObjectMap;
+
+public class ENWIAM90 extends TestBase {
+	static int count = -1;
+
+	static boolean fail = false;
+	static boolean skip = false;
+	static int status = 1;
+	static String followBefore = null;
+	static String followAfter = null;
+
+	/**
+	 * Method for displaying JIRA ID's for test case in specified path of Extent
+	 * Reports
+	 * 
+	 * @throws Exception
+	 *             , When Something unexpected
+	 */
+
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		extent = ExtentManager.getReporter(filePath);
+		rowData = testcase.get(this.getClass().getSimpleName());
+		test = extent.startTest(rowData.getTestcaseId(), rowData.getTestcaseDescription()).assignCategory("ENWIAM");
+	}
+
+	/**
+	 * Method for login into Neon application using TR ID
+	 * 
+	 * @throws Exception
+	 *             , When TR Login is not done
+	 */
+	@Test
+	public void testcaseENWIAM90() throws Exception {
+		boolean testRunmode = getTestRunMode(rowData.getTestcaseRunmode());
+		boolean master_condition = suiteRunmode && testRunmode;
+		logger.info("checking master condition status-->" + this.getClass().getSimpleName() + "-->" + master_condition);
+
+		if (!master_condition) {
+			status = 3;
+			test.log(LogStatus.SKIP,
+					"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
+			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
+		}
+		try {
+			String statuCode = deleteUserAccounts(LOGIN.getProperty("FACEBOOKACCOUNT"));
+			logger.info("statuCode -->"+statuCode);
+			if (!(statuCode.equalsIgnoreCase("200") || statuCode.equalsIgnoreCase("400"))) {
+				throw new Exception("Delete API Call failed");
+			}
+		} catch (Throwable t) {
+			test.log(LogStatus.FAIL, "Delete accounts api call failed");// extent
+			ErrorUtil.addVerificationFailure(t);
+		}
+
+		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts ");
+		try {
+			String accountType = "";
+			openBrowser();
+			maximizeWindow();
+			clearCookies();
+			ob.get(host + CONFIG.getProperty("appendENWAppUrl"));
+			pf.getOnboardingModalsPageInstance(ob).ENWSTeamLogin(LOGIN.getProperty("FACEBOOKACCOUNT"),
+					(LOGIN.getProperty("FACEBOOKACCOUNTPWD")));
+			BrowserWaits.waitTime(3);
+			jsClick(ob, ob.findElement(By.xpath(OnePObjectMap.ENW_PROFILE_USER_ICON_XPATH.toString())));
+			waitForElementTobeVisible(ob, By.xpath(OnePObjectMap.ENDNOTE_ACCOUNT_LINK_XPATH.toString()), 30);
+			ob.findElement(By.xpath(OnePObjectMap.ENDNOTE_ACCOUNT_LINK_XPATH.toString())).click();
+			BrowserWaits.waitTime(4);
+			pf.getLoginTRInstance(ob).closeOnBoardingModal();
+			String firstAccountProfileName = pf.getLinkingModalsInstance(ob).getProfileName();
+			test.log(LogStatus.INFO, "Social account profile name: " + firstAccountProfileName);
+			pf.getHFPageInstance(ob).clickProfileImage();
+			pf.getHFPageInstance(ob).clickOnAccountLink();
+			accountType = "Neon";
+			validateAccounts(1, accountType);
+			BrowserWaits.waitTime(5);
+			jsClick(ob, ob.findElement(By.cssSelector(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_IMAGE_CSS.toString())));
+			jsClick(ob, ob.findElement(By.linkText(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_SIGNOUT_LINK.toString())));
+			BrowserWaits.waitTime(10);
+			ob.get(host + CONFIG.getProperty("appendENWAppUrl"));
+			try {
+				pf.getLoginTRInstance(ob).loginWithFBCredentials(LOGIN.getProperty("FACEBOOKACCOUNT"),
+						LOGIN.getProperty("FACEBOOKACCOUNTPWD"));
+				test.log(LogStatus.PASS, "user has logged in with social account");
+				BrowserWaits.waitTime(5);
+				pf.getENWReferencePageInstance(ob).didYouKnow(LOGIN.getProperty("FACEBOOKACCOUNTPWD"));
+				test.log(LogStatus.PASS, "user is able to link");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			pf.getENWReferencePageInstance(ob).clickAccount();
+			pf.getENWReferencePageInstance(ob).closeOnBoardingModal();
+			pf.getHFPageInstance(ob).clickOnAccountLink();
+			accountType = "Facebook";
+			try {
+				validateLinkedAccounts(2, accountType);
+				String secondAccountProfileName = pf.getLinkingModalsInstance(ob).getProfileName();
+				test.log(LogStatus.INFO, "After merging Steam account profile name: " + secondAccountProfileName);
+				Assert.assertEquals(firstAccountProfileName, secondAccountProfileName);
+				test.log(LogStatus.PASS, "Forward Merge is happened");
+
+			} catch (Exception e) {
+				test.log(LogStatus.FAIL, "Forward Merge is not happened");// extent
+				// reports
+				status = 2;// excel
+				test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(captureScreenshot(this.getClass()
+						.getSimpleName()
+						+ "_more_search_results_do_not_get_displayed_when_user_scrolls_down_in_ALL_search_results_page")));// screenshot
+				ErrorUtil.addVerificationFailure(e);
+			}
+
+		} catch (Throwable t) {
+			test.log(LogStatus.FAIL, "Something unexpected happened");// extent
+			// reports
+			// next 3 lines to print whole testng error in report
+			StringWriter errors = new StringWriter();
+			t.printStackTrace(new PrintWriter(errors));
+			test.log(LogStatus.INFO, errors.toString());// extent reports
+			ErrorUtil.addVerificationFailure(t);// testng
+			test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(
+					captureScreenshot(this.getClass().getSimpleName() + "_something_unexpected_happened")));// screenshot
+
+		}
+		pf.getHFPageInstance(ob).clickProfileImage();
+		jsClick(ob, ob.findElement(By.xpath(OR.getProperty("signOut_link"))));
+		closeBrowser();
+	}
+
+	private void validateLinkedAccounts(int accountCount, String linkName) throws Exception {
+		try {
+
+			Assert.assertTrue(
+					pf.getAccountPageInstance(ob).verifyLinkedAccount("Neon", LOGIN.getProperty("FACEBOOKACCOUNT")));
+			Assert.assertTrue(
+					pf.getAccountPageInstance(ob).verifyLinkedAccount(linkName, LOGIN.getProperty("FACEBOOKACCOUNT")));
+			Assert.assertTrue(pf.getAccountPageInstance(ob).validateAccountsCount(accountCount));
+			test.log(LogStatus.PASS,
+					"Linked accounts are available in accounts page : Neon and " + linkName + " accounts");
+
+		} catch (Throwable t) {
+			test.log(LogStatus.FAIL,
+					"Linked accounts are not available in accounts page : Neon and " + linkName + " accounts");
+			ErrorUtil.addVerificationFailure(t);// testng
+			test.log(LogStatus.INFO, "Snapshot below: "
+					+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName() + "Linking_failed")));// screenshot
+		}
+	}
+
+	private void validateAccounts(int accountCount, String linkName) throws Exception {
+		try {
+
+			Assert.assertTrue(
+					pf.getAccountPageInstance(ob).verifyLinkedAccount(linkName, LOGIN.getProperty("FACEBOOKACCOUNT")));
+			Assert.assertTrue(pf.getAccountPageInstance(ob).validateAccountsCount(accountCount));
+			test.log(LogStatus.PASS, "Single Social account is available and is not linked to Any account");
+
+		} catch (Throwable t) {
+			test.log(LogStatus.FAIL,
+					"Linked accounts are available in accounts page : Neon and " + linkName + " accounts");
+			ErrorUtil.addVerificationFailure(t);// testng
+			test.log(LogStatus.INFO, "Snapshot below: "
+					+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName() + "_failed")));// screenshot
+		}
+	}
+
+	@AfterTest
+	public void reportTestResult() {
+		extent.endTest(test);
+
+	}
+
+}
