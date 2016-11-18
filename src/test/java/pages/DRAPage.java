f45a957997f98f2f1088698d2cd58638edff193b
Sruthi Patluri <Sruthi.Patluri@thomsonreuters.com>
2016-11-18 15:35:28 +0530

    Added DRA Page

--- src/test/java/pages/DRAPage.java
+++ src/test/java/pages/DRAPage.java
@@ -1,24 +1,7 @@
 package pages;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
-
-import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
 
 import base.TestBase;
 import util.BrowserWaits;
