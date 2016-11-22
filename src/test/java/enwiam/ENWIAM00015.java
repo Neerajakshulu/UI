f06ff5aac7d3c105c3383fb6ca6f5a4f0f056a05
Chinna Putha <chinna.putha@thomsonreuters.com>
2016-11-22 14:23:17 +0530

    code changes for ENWIAM script

--- src/test/java/enwiam/ENWIAM00015.java
+++ src/test/java/enwiam/ENWIAM00015.java
@@ -13,8 +13,6 @@ import pages.PageFactory;
 import util.BrowserWaits;
 import util.ErrorUtil;
 import util.ExtentManager;
-import util.OnePObjectMap;
-import util.TestUtil;
 
 // OPQA-2389 -Verify that,when STeAM account is trying to be linked by the user is in a "locked" status,
 //then the link should not be made and the user should be informed that the STeAM account is locked.
@@ -44,7 +42,7 @@ public class ENWIAM00015 extends TestBase {
 	@Test
 	public void testLogin() throws Exception {
 
-		boolean testRunmode = TestUtil.isTestCaseRunnable(enwiamxls, this.getClass().getSimpleName());
+		boolean testRunmode = getTestRunMode(rowData.getTestcaseRunmode());
 		boolean master_condition = suiteRunmode && testRunmode;
 
 		if (!master_condition) {
@@ -76,59 +74,7 @@ public class ENWIAM00015 extends TestBase {
 		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution ends--->");
 	}
 
-	private void steamLocked() throws Exception {
-		String str = "Your account has been locked.";
-		String locked = "";
-		ob.navigate().to(host + CONFIG.getProperty("appendENWAppUrl"));
-
-		ob.findElement(By.cssSelector(OnePObjectMap.LOGIN_PAGE_EMAIL_TEXT_BOX_CSS.toString())).clear();
-		ob.findElement(By.cssSelector(OnePObjectMap.LOGIN_PAGE_EMAIL_TEXT_BOX_CSS.toString()))
-				.sendKeys(LOGIN.getProperty("ENWIAM00015steamUser"));
-
-		for (int i = 0; i <= 10; i++) {
-			ob.findElement(By.name("loginPassword")).sendKeys("asdfgh");
-			ob.findElement(By.cssSelector(OnePObjectMap.LOGIN_PAGE_PASSWORD_TEXT_BOX_CSS.toString()))
-					.sendKeys("asdfgh");
-			pf.getBrowserActionInstance(ob).jsClick(OnePObjectMap.LOGIN_PAGE_SIGN_IN_BUTTON_CSS);
-			Thread.sleep(2000);
-		}
-		BrowserWaits.waitTime(4);
-		waitForElementTobeVisible(ob, By.cssSelector(OnePObjectMap.ENW_UNVERIFIED_MESSAGE_BUTTON_CSS.toString()), 30);
-		locked = ob.findElement(By.cssSelector(OnePObjectMap.ENW_UNVERIFIED_MESSAGE_BUTTON_CSS.toString())).getText();	
-		BrowserWaits.waitTime(2);
-		if (locked.equalsIgnoreCase(str)) {
-			test.log(LogStatus.PASS, "The locked string is displayed, the account got locked on ENW");
-		}
-
-		else {
-			test.log(LogStatus.FAIL, "The locked string is not displayed, the account is not locked on ENW");
-		}
-
-
-	}
-	private void loginTofb() throws Exception {
-
-		// Navigate to TR login page and login with valid TR credentials
-		ob.navigate().to(host + CONFIG.getProperty("appendENWAppUrl"));
-
-		String str = "Your account has been locked.";
-		String locked = "";
-		pf.getLoginTRInstance(ob).loginWithFBCredentials(LOGIN.getProperty("ENWIAM00015steamUser"),
-				LOGIN.getProperty("ENWIAM00015steamUserPWD"));
-		pf.getENWReferencePageInstance(ob).didYouKnow(LOGIN.getProperty("ENWIAM00015steamUserPWD"));
-		BrowserWaits.waitTime(2);
-		locked = ob.findElement(By.xpath(OnePObjectMap.LOCKED_MSG_XPATH.toString())).getText();
-
-		if (locked.equalsIgnoreCase(str)) {
-			test.log(LogStatus.PASS, "STeAM account is in a locked status and can not be linked");
-
-		} else {
-			test.log(LogStatus.FAIL, "STeAM account is not in a locked status");
-		}
-		waitForElementTobeVisible(ob, By.xpath(OnePObjectMap.LOCKED_OK_BUTTON_XPATH.toString()), 30);
-		ob.findElement(By.xpath(OnePObjectMap.LOCKED_OK_BUTTON_XPATH.toString())).click();
-	}
-
+	
 	// LOGIN TO ENW AND CHECKING THE EVICTED ACCONT
 	private void steamEnw() throws Exception {
 		ob.navigate().to(host + CONFIG.getProperty("appendENWAppUrl"));
