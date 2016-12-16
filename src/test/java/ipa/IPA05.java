a0ea5909071e9bac69ec8e8bbbb253276d0d7676
Kiran RN <kiran.rn@thomsonreuters.com>
2016-12-16 13:29:19 +0530

    modified in IPA module

--- src/test/java/ipa/IPA05.java
+++ src/test/java/ipa/IPA05.java
@@ -79,7 +79,7 @@ public class IPA05 extends TestBase {
 					throw new Exception("Login not sucess");				
 			test.log(LogStatus.PASS, "Login successfully");
 			pf.getDashboardPage(ob).SearchTermEnter("company", "Apple");
-			pf.getDashboardPage(ob).addCompanyTerms("2:1");
+			pf.getDashboardPage(ob).addCompanyTerms("1");
 			pf.getDashboardPage(ob).exploreSearch();
 			pf.getDashboardPage(ob).validateCompanyKeyInformationPanel(test);
 			
