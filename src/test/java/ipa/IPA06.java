8faee0fddc97ffa968c72d307468291d0eef310b
Kiran RN <kiran.rn@thomsonreuters.com>
2016-12-19 11:38:14 +0530

    heloo

--- src/test/java/ipa/IPA06.java
+++ src/test/java/ipa/IPA06.java
@@ -14,7 +14,7 @@ import com.relevantcodes.extentreports.LogStatus;
 
 import base.TestBase;
 import util.BrowserAction;
-import util.ErrorUtil ; 
+import util.ErrorUtil; 
 import util.ExtentManager;
 import util.OnePObjectMap;
 import util.TestUtil;
