b0c59f8a9d3a3df5f160c920938d9a63ada8545c
salma-sadia <salma.sadik@thomsonreuters.com>
2017-01-12 16:59:36 +0530

    fixed watchlist014

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -1570,7 +1570,7 @@ public class TestBase {
 		waitForElementTobeClickable(ob, By.xpath(OR.getProperty("createWatchListButton1")), 60);
 		ob.findElement(By.xpath(OR.getProperty("createWatchListButton1"))).click();
 		BrowserWaits.waitTime(4);
-		waitForElementTobeClickable(ob, By.xpath(OR.getProperty("newWatchListNameTextBox")), 60);
+		waitForElementTobeClickable(ob, By.xpath(OR.getProperty("newWatchListNameTextBox")), 120);
 		ob.findElement(By.xpath(OR.getProperty("newWatchListNameTextBox"))).sendKeys(watchListName);
 		BrowserWaits.waitTime(5);
 		waitForElementTobeClickable(ob, By.xpath(OR.getProperty("newWatchListDescriptionTextArea")), 60);
