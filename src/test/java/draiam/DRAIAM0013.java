e3faf5c5c238592ede5d92dd0afcfe6cfce4850a
Naznina Begum <naznina.begum@thomsonreuters.com>
2017-01-19 15:33:29 +0530

    Fixed DRAIAM script

--- src/test/java/draiam/DRAIAM0013.java
+++ src/test/java/draiam/DRAIAM0013.java
@@ -85,13 +85,7 @@ public class DRAIAM0013 extends TestBase {
 				ErrorUtil.addVerificationFailure(t);
 			}
 
-			waitForElementTobeVisible(ob, By.cssSelector(OnePObjectMap.DRA_FORGOT_PASSWORD_LINK_CSS.toString()), 30);
-			ob.findElement(By.cssSelector(OnePObjectMap.DRA_FORGOT_PASSWORD_LINK_CSS.toString()))
-					.sendKeys(LOGIN.getProperty("DRASteamuser14"));
-			ob.findElement(By.cssSelector(OnePObjectMap.DRA_FORGOT_PASSWORD_VERIFICATION_BUTTON_CSS.toString()))
-					.click();
-			test.log(LogStatus.PASS, "'Send verification button' is clicked");
-			BrowserWaits.waitTime(3);
+			
 			ob.close();
 
 		} catch (Throwable t) {
