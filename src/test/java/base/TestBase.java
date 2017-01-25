807070ab4a7bf0f14e530862a60ecb0f1e04cbd9
Chinna Putha <chinna.putha@thomsonreuters.com>
2017-01-25 12:50:37 +0530

    Made changes in Testbase to dispaly Testcase name in saucelabs dashboard

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -436,6 +436,7 @@ public class TestBase {
 			desiredCapabilities.setCapability(CapabilityType.PLATFORM, System.getenv("SELENIUM_PLATFORM"));
 			desiredCapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true); //
 			desiredCapabilities.setCapability(CapabilityType.HAS_NATIVE_EVENTS, true);
+			desiredCapabilities.setCapability("name", this.getClass().getSimpleName());
 			ob = new RemoteWebDriver(
 					new URL("http://amneetsingh:f48a9e78-a431-4779-9592-1b49b6d406a4@ondemand.saucelabs.com:80/wd/hub"),
 					desiredCapabilities);
@@ -450,9 +451,9 @@ public class TestBase {
 			// else part having local machine configuration
 		} else {
 			logger.info("Running Environment is Local Machine");
-			if (CONFIG.getProperty("browserType").equals("FF")) {
+			if (CONFIG.getProperty("browserType").equalsIgnoreCase("FF")) {
 				ob = new FirefoxDriver();
-			} else if (CONFIG.getProperty("browserType").equals("IE")) {
+			} else if (CONFIG.getProperty("browserType").equalsIgnoreCase("IE")) {
 				DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
 				capabilities.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);
 				System.setProperty("webdriver.ie.driver", "drivers/IEDriverServer.exe");
@@ -513,28 +514,27 @@ public class TestBase {
 
 		DesiredCapabilities caps = null;
 
-		if (os.equals("Windows")) {
+		if (os.equalsIgnoreCase("Windows")) {
 
-			if (browser.equals("Chrome")) {
-
+			if (browser.equalsIgnoreCase("Chrome")) {
 				caps = DesiredCapabilities.chrome();
 				caps.setCapability("platform", "Windows 7");
 				caps.setCapability("version", "48.0");
+				caps.setCapability("name", this.getClass().getSimpleName());
 			}
 
-			if (browser.equals("FF")) {
-
+			if (browser.equalsIgnoreCase("FF")) {
 				caps = DesiredCapabilities.firefox();
 				caps.setCapability("platform", "Windows 7");
 				caps.setCapability("version", "44.0");
-
+				caps.setCapability("name", this.getClass().getSimpleName());
 			}
 
-			if (browser.equals("IE")) {
-
+			if (browser.equalsIgnoreCase("IE")) {
 				caps = DesiredCapabilities.internetExplorer();
 				caps.setCapability("platform", "Windows 8.1");
 				caps.setCapability("version", "11.0");
+				caps.setCapability("name", this.getClass().getSimpleName());
 
 			}
 
