60d19e67aa667b2dde5f0126f2e70974d8bec3ad
Naznina Begum <naznina.begum@thomsonreuters.com>
2017-01-30 12:23:27 +0530

    Modified ENWIAM script

--- src/test/java/enwiam/ENWIAM54.java
+++ src/test/java/enwiam/ENWIAM54.java
@@ -61,7 +61,7 @@ public class ENWIAM54 extends TestBase {
 			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
 		}
 
-		/*try {
+		try {
 			String statuCode = deleteUserAccounts(LOGIN.getProperty("sru_fbusername08"));
 			String statuCode2 = deleteUserAccounts(LOGIN.getProperty("sru_steam08"));
 			if (!(statuCode.equalsIgnoreCase("200") || statuCode.equalsIgnoreCase("400"))) {
@@ -77,7 +77,7 @@ public class ENWIAM54 extends TestBase {
 		} catch (Throwable t) {
 			test.log(LogStatus.FAIL, "Delete accounts api call failed");// extent
 			ErrorUtil.addVerificationFailure(t);
-		}*/
+		}
 
 		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts ");
 
