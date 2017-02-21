b5d7334ae7d5a371e901e4de366c37269a64b85b
Naznina Begum <naznina.begum@thomsonreuters.com>
2017-02-21 12:45:50 +0530

    modified DRAIAM Script

--- src/test/java/pages/DRAPage.java
+++ src/test/java/pages/DRAPage.java
@@ -334,7 +334,7 @@ public class DRAPage extends TestBase {
 
 			}
 
-			if ((accountType.equalsIgnoreCase("Steam") && type1.contains("Clarivate Analytics"))
+			if ((accountType.equalsIgnoreCase("Steam") && type1.contains("Clarivate Analytics")||type1.contains("Thomson Reuters | Project Neon"))
 					|| accountType.equalsIgnoreCase(type1.trim())) {
 				String emailid = null;
 				emailid = element.findElement(By.cssSelector("span[class='ng-binding']")).getText();
