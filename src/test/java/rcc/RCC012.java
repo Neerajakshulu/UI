931bdcbeeabbc07be8b62a6783bdf99c5eb8edcb
Asif <Mohammed.AsifA@thomsonreuters.com>
2017-03-08 17:13:51 +0530

    Modified RCC012

--- src/test/java/rcc/RCC012.java
+++ src/test/java/rcc/RCC012.java
@@ -69,7 +69,7 @@ public class RCC012 extends TestBase{
 			pf.getGroupsPage(ob).clickOnCreateNewGroupButton();
 			pf.getGroupsListPage(ob).createGroup(title, desc);
 			test.log(LogStatus.INFO, "Group is created successfully: " + title);
-			BrowserWaits.waitTime(20);
+			BrowserWaits.waitTime(22);
 			try {
 				Assert.assertEquals(pf.getGroupDetailsPage(ob).getGroupTitle(), title);
 				test.log(LogStatus.PASS, "Group title displayed in Group details page correctly");
