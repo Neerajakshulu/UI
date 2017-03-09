f576e2b1df57d12eaee1813e160f0264d8877564
Asif <Mohammed.AsifA@thomsonreuters.com>
2017-03-09 17:07:13 +0530

    Modified RCC006

--- src/test/java/rcc/RCC006.java
+++ src/test/java/rcc/RCC006.java
@@ -196,7 +196,7 @@ public class RCC006 extends TestBase{
 				test.log(LogStatus.FAIL, "Owner name not same");
 			}*/
 
-			BrowserWaits.waitTime(2);
+			BrowserWaits.waitTime(3);
 			pf.getLoginTRInstance(ob).logOutApp();
 			loginAs("RCCTESTUSER025", "RCCTESTUSERPWD025");
 
