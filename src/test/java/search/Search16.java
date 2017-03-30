be8e691d4cc02ca8d6fad6dd539762133e24780f
UC226534 <UC226534@UC226534-TPL-A.ten.thomsonreuters.com>
2017-03-30 11:16:23 +0530

    modified locators in oneobjectmap and properties file

--- src/test/java/search/Search16.java
+++ src/test/java/search/Search16.java
@@ -17,6 +17,7 @@ import base.TestBase;
 import util.BrowserWaits;
 import util.ErrorUtil;
 import util.ExtentManager;
+import util.OnePObjectMap;
 
 public class Search16 extends TestBase {
 
@@ -64,20 +65,22 @@ public class Search16 extends TestBase {
 			clearCookies();
 
 			// Navigate to TR login page and login with valid TR credentials
-			// ob.navigate().to(host);
-			ob.navigate().to(CONFIG.getProperty("testSiteName"));
+			ob.navigate().to(host);
 			login();
 			waitForElementTobeVisible(ob, By.cssSelector(OR.getProperty("tr_search_box_css")), 20);
 			ob.findElement(By.cssSelector(OR.getProperty("tr_search_box_css"))).sendKeys("biology", Keys.ENTER);
 			waitForAjax(ob);
-			waitForElementTobeClickable(ob, By.cssSelector(OR.getProperty("tr_search_results_sortby_button_css")), 20);
-			jsClick(ob, ob.findElement(By.cssSelector(OR.getProperty("tr_search_results_sortby_button_css"))));
+			waitForElementTobeClickable(ob, By.cssSelector(OnePObjectMap.HOME_PROJECT_NEON_SEARCH_CLICK_CSS.toString()),
+					20);
+			ob.findElement(By.cssSelector(OnePObjectMap.HOME_PROJECT_NEON_SEARCH_CLICK_CSS.toString())).click();
+			waitForAjax(ob);
+			jsClick(ob, ob.findElement(By.cssSelector(OnePObjectMap.SEARCH_RESULT_PAGE_SORT_DROPDOWN_CSS.toString())));
 			BrowserWaits.waitTime(2);
-			waitForElementTobeVisible(ob, By.cssSelector(OR.getProperty("tr_search_results_sortby_menu_css")), 20);
+			waitForElementTobeVisible(ob, By.cssSelector(OnePObjectMap.SEARCH_RESULT_PAGE_SORT_DROPDOWN_ITEMS_CSS.toString()), 20);
 
 			try {
 				Assert.assertTrue(checkElementIsDisplayed(ob,
-						By.cssSelector(OR.getProperty("tr_search_results_sortby_menu_css"))));
+						By.cssSelector(OnePObjectMap.SEARCH_RESULT_PAGE_SORT_DROPDOWN_ITEMS_CSS.toString())));
 				test.log(LogStatus.PASS, "user is able to expand the sort by menu for search results");
 			} catch (Throwable t) {
 				test.log(LogStatus.FAIL, "Sort by menu is not getting expanded in search results page");
@@ -87,11 +90,11 @@ public class Search16 extends TestBase {
 				test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(captureScreenshot(
 						this.getClass().getSimpleName() + "sortby_ is_not_ working_ for_ search_ results")));// screenshot
 			}
-			ob.findElement(By.cssSelector(OR.getProperty("tr_search_results_sortby_menu_css"))).click();
+			ob.findElement(By.cssSelector(OnePObjectMap.SEARCH_RESULT_PAGE_SORT_DROPDOWN_ITEMS_CSS.toString())).click();
 
 			try {
 				Assert.assertFalse(checkElementIsDisplayed(ob,
-						By.cssSelector(OR.getProperty("tr_search_results_sortby_menu_css"))));
+						By.cssSelector(OnePObjectMap.SEARCH_RESULT_PAGE_SORT_DROPDOWN_ITEMS_CSS.toString())));
 				test.log(LogStatus.PASS, "User is able to collapse sort by menu for search results");
 			} catch (Throwable t) {
 				test.log(LogStatus.FAIL, "Sort by menu is not getting collapsed in search results page");
@@ -129,13 +132,16 @@ public class Search16 extends TestBase {
 
 		// if (status == 1)
 		// TestUtil.reportDataSetResult(searchxls, "Test Cases",
-		// TestUtil.getRowNum(searchxls, this.getClass().getSimpleName()), "PASS");
+		// TestUtil.getRowNum(searchxls, this.getClass().getSimpleName()),
+		// "PASS");
 		// else if (status == 2)
 		// TestUtil.reportDataSetResult(searchxls, "Test Cases",
-		// TestUtil.getRowNum(searchxls, this.getClass().getSimpleName()), "FAIL");
+		// TestUtil.getRowNum(searchxls, this.getClass().getSimpleName()),
+		// "FAIL");
 		// else
 		// TestUtil.reportDataSetResult(searchxls, "Test Cases",
-		// TestUtil.getRowNum(searchxls, this.getClass().getSimpleName()), "SKIP");
+		// TestUtil.getRowNum(searchxls, this.getClass().getSimpleName()),
+		// "SKIP");
 
 	}
 
