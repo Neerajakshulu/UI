808035d301d356d835a0cf3198e44fad54970b8a
bhavyasree-gogineni <bhavyasree.goginnei@thomsonreuters.com>
2017-05-15 13:40:28 +0530

    Added new test case to search module

--- src/test/java/search/Search131.java
+++ src/test/java/search/Search131.java
@@ -20,7 +20,7 @@ import util.ErrorUtil;
 import util.ExtentManager;
 import util.OnePObjectMap;
 
-public class Search18 extends TestBase {
+public class Search131 extends TestBase {
 
 	static int status = 1;
 
@@ -30,24 +30,24 @@ public class Search18 extends TestBase {
 	// 3--->SKIP
 	// Checking whether this test case should be skipped or not
 	@BeforeTest
-	public void beforeTest() throws Exception {
+	public void beforeTest113() throws Exception {
 		extent = ExtentManager.getReporter(filePath);
 		rowData = testcase.get(this.getClass().getSimpleName());
-		test = extent.startTest(rowData.getTestcaseId(), rowData.getTestcaseDescription())
-				.assignCategory("Search suite");
-
+		test = extent.startTest(rowData.getTestcaseId(), rowData.getTestcaseDescription()).assignCategory(
+				"Search suite");
 	}
 
 	@Test
-	public void testcaseB18() throws Exception {
+	public void testcaseB() throws Exception {
+
 		boolean testRunmode = getTestRunMode(rowData.getTestcaseRunmode());
 		boolean master_condition = suiteRunmode && testRunmode;
 
 		if (!master_condition) {
 
 			status = 3;// excel
-			test.log(LogStatus.SKIP,
-					"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
+			test.log(LogStatus.SKIP, "Skipping test case " + this.getClass().getSimpleName()
+					+ " as the run mode is set to NO");
 			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
 
 		}
@@ -64,24 +64,79 @@ public class Search18 extends TestBase {
 				System.out.println("maximize() command not supported in Selendroid");
 			}
 			clearCookies();
+
 			// Navigate to TR login page and login with valid TR credentials
 			ob.navigate().to(host);
-			// ob.navigate().to(CONFIG.getProperty("testSiteName"));
-
 			login();
-			// waitForElementTobeVisible(ob, By.cssSelector(OR.getProperty("")), 20);
-			// ob.findElement(By.cssSelector(OR.getProperty("tr_search_box_css"))).sendKeys("biology");
-			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("searchBox_textBox")), 30);
-			ob.findElement(By.xpath(OR.getProperty("searchBox_textBox"))).sendKeys("biology");
-			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("search_button")), 20);
+			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("search_button")), 30);
+			ob.findElement(By.xpath(OR.getProperty("searchBox_textBox"))).sendKeys("post");
 			ob.findElement(By.xpath(OR.getProperty("search_button"))).click();
-			waitForAjax(ob);
-			pf.getSearchResultsPageInstance(ob).clickOnArticleTab();
-			waitForAllElementsToBePresent(ob, By.cssSelector(OR.getProperty("tr_search_results_refine_expand_css")),
-					40);
-			ob.findElement(By.cssSelector(OR.getProperty("tr_search_results_refine_expand_css"))).click();
-			BrowserWaits.waitTime(5);
+			BrowserWaits.waitTime(3);
+			pf.getSearchResultsPageInstance(ob).clickOnPostTab();
+			waitForElementTobeVisible(ob, By.xpath(OR.getProperty("searchResults_links")), 30);
+			Thread.sleep(2000);
+			waitForAllElementsToBePresent(ob, By.cssSelector("div[class='panel-heading']"), 30);
+			String fname = ob.findElement(By.cssSelector(" h4[class='panel-title'] div[class='ng-scope'] span"))
+					.getText();
+			if (compareStrings("Institutions", fname))
+				test.log(LogStatus.PASS, "Institution filter is present in Posts search result page ");// extent
+			// reports
 
+			else {
+				test.log(LogStatus.FAIL, "Less link not working");// extent
+				// reports
+				status = 2;// excel
+				test.log(
+						LogStatus.INFO,
+						"Snapshot below: "
+								+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName()
+										+ "_more_link_not_working")));// screenshot
+
+			}
+
+			List<WebElement> filter_list = ob.findElements(By.cssSelector("div[class='panel-heading']"));
+			filter_list.get(0).click();
+			BrowserWaits.waitTime(4);
+			waitForElementTobeVisible(ob,
+					By.xpath("//button[@class='search-result-refine-menu__load-button ng-scope']"), 30);
+			jsClick(ob, ob.findElement(By.xpath("//button[@class='search-result-refine-menu__load-button ng-scope']")));
+			BrowserWaits.waitTime(2);
+
+			// System.out.println(ob.findElement(By.xpath("//button[@class='load-more-button ng-scope']")).getText());
+
+			String temp1 = ob.findElement(
+					By.xpath("//button[@class='search-result-refine-menu__load-button ng-scope']")).getText();
+			if (!compareStrings("Less", temp1)) {
+
+				test.log(LogStatus.FAIL, "Less link not working");// extent
+				// reports
+				status = 2;// excel
+				test.log(
+						LogStatus.INFO,
+						"Snapshot below: "
+								+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName()
+										+ "_more_link_not_working")));// screenshot
+
+			}
+		
+			jsClick(ob, ob.findElement(By.xpath("//button[@class='search-result-refine-menu__load-button ng-scope']")));
+			BrowserWaits.waitTime(2);
+			String temp2 = ob.findElement(
+					By.xpath("//button[@class='search-result-refine-menu__load-button ng-scope']")).getText();
+
+			if (!compareStrings("More", temp2)) {
+
+				test.log(LogStatus.FAIL, "More link not working");// extent
+				// reports
+				status = 2;// excel
+				test.log(
+						LogStatus.INFO,
+						"Snapshot below: "
+								+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName()
+										+ "_less_link_not_working")));// screenshot
+
+			}
+			
 			int checkboxesSelected = 0;
 			List<WebElement> checkboxList;
 			for (int i = 0; i < 2; i++) {
@@ -120,9 +175,9 @@ public class Search18 extends TestBase {
 
 			try {
 				Assert.assertTrue(checkboxesSelected == 0);
-				test.log(LogStatus.PASS, "Reset button for search results should work fine");
+				test.log(LogStatus.PASS, "Reset button for post search results should work fine");
 			} catch (Throwable t) {
-				test.log(LogStatus.FAIL, "Reset button for search results not working as expected");
+				test.log(LogStatus.FAIL, "Reset button for post search results not working as expected");
 				test.log(LogStatus.INFO, "Error--->" + t);
 				ErrorUtil.addVerificationFailure(t);
 				status = 2;
@@ -141,8 +196,11 @@ public class Search18 extends TestBase {
 			test.log(LogStatus.INFO, errors.toString());// extent reports
 			ErrorUtil.addVerificationFailure(t);// testng
 			status = 2;// excel
-			// test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture(
-			// captureScreenshot(this.getClass().getSimpleName() + "_something_unexpected_happened")));// screenshot
+			test.log(
+					LogStatus.INFO,
+					"Snapshot below: "
+							+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName()
+									+ "_something_unexpected_happened")));// screenshot
 			closeBrowser();
 		}
 
@@ -153,15 +211,15 @@ public class Search18 extends TestBase {
 	public void reportTestResult() {
 		extent.endTest(test);
 
-		// if (status == 1)
+		// if(status==1)
 		// TestUtil.reportDataSetResult(searchxls, "Test Cases",
-		// TestUtil.getRowNum(searchxls, this.getClass().getSimpleName()), "PASS");
-		// else if (status == 2)
+		// TestUtil.getRowNum(searchxls,this.getClass().getSimpleName()), "PASS");
+		// else if(status==2)
 		// TestUtil.reportDataSetResult(searchxls, "Test Cases",
-		// TestUtil.getRowNum(searchxls, this.getClass().getSimpleName()), "FAIL");
+		// TestUtil.getRowNum(searchxls,this.getClass().getSimpleName()), "FAIL");
 		// else
 		// TestUtil.reportDataSetResult(searchxls, "Test Cases",
-		// TestUtil.getRowNum(searchxls, this.getClass().getSimpleName()), "SKIP");
+		// TestUtil.getRowNum(searchxls,this.getClass().getSimpleName()), "SKIP");
 
 	}
 
