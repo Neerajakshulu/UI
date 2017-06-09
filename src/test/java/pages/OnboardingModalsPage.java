06aa1e8a559c19e8dcaec05a1d11c68a1f01b08c
Chinna Putha <chinna.putha@thomsonreuters.com>
2017-06-09 12:16:03 +0530

    Profile local run script fixes

--- src/test/java/pages/OnboardingModalsPage.java
+++ src/test/java/pages/OnboardingModalsPage.java
@@ -717,7 +717,7 @@ public class OnboardingModalsPage extends TestBase {
 				.getElements(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_MODAL_CSS);
 		logger.info("onboarding status-->" + onboardingStatus.size());
 
-		try {
+		/*try {*/
 			pf.getBrowserWaitsInstance(ob)
 					.waitUntilElementIsClickable(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_WELCOME_MODAL_CSS);
 			pf.getBrowserWaitsInstance(ob).waitUntilText("We’re expanding EndNote -- making it easier for you to:");
@@ -741,9 +741,9 @@ public class OnboardingModalsPage extends TestBase {
 					"Followers", "Following", "Watchlists", "Add a Topic");
 			logger.info("Navigate to ENW-->"+pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.NEON_TO_ENW_BACKTOENDNOTE_LINK).getText());
 			Assert.assertEquals("< Back to EndNote", pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.NEON_TO_ENW_BACKTOENDNOTE_LINK).getText());
-		} catch (Exception e) {
+		/*} catch (Exception e) {
 			throw new Exception("Onboarding Modals are not displayed for First time user using Account link for Market Test group");
-		}
+		}*/
 
 	}
 	
@@ -838,7 +838,7 @@ public class OnboardingModalsPage extends TestBase {
 				.getElements(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_MODAL_CSS);
 		logger.info("onboarding status-->" + onboardingStatus.size());
 
-		try {
+		/*try {*/
 			profileOnboardingModal();
 			//Profile image modal
 			pf.getBrowserActionInstance(ob).jsClick(OnePObjectMap.HOME_PROJECT_NEON_ONBOARDING_PROFILE_MODAL_IMAGE_EDIT_CSS);
@@ -857,9 +857,9 @@ public class OnboardingModalsPage extends TestBase {
 			pf.getBrowserWaitsInstance(ob).waitUntilText("Add a Topic","You can always complete your profile later from  your Profile page.");
 			pf.getBrowserWaitsInstance(ob).waitUntilText("Note:", "Your name and any additional information you add will", "be visible to others.");
 			
-		} catch (Exception e) {
+		/*} catch (Exception e) {
 			throw new Exception("Profile Image upload not done in Profile modal");
-		}
+		}*/
 
 	}
 	
