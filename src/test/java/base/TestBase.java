98497341fb2f0c9806388ff41c1efc15315fbd80
Chinna Putha <chinna.putha@thomsonreuters.com>
2017-06-13 22:38:17 +0530

    Saucelabs credentials updated

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -442,7 +442,7 @@ public class TestBase {
 				}
 			}
 			ob = new RemoteWebDriver(
-					new URL("http://amneetsingh:f48a9e78-a431-4779-9592-1b49b6d406a4@ondemand.saucelabs.com:80/wd/hub"),
+					new URL("http://chinna_putha:e58d38dc-3d93-43e8-af25-5e3a1df25e8b@ondemand.saucelabs.com:80/wd/hub"),
 					desiredCapabilities);
 			String waitTime = CONFIG.getProperty("defaultImplicitWait");
 			String pageWait = CONFIG.getProperty("defaultPageWait");
