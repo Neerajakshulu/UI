aa25da65133b6d1314652138415a70313d38d1b4
Krishnaraja Upadhyaya <krishnaraja.upadhyaya@thomsonreuters.com>
2017-09-01 15:26:31 +0530

    Adds new testscript WAT46

--- src/test/java/util/OnePObjectMap.java
+++ src/test/java/util/OnePObjectMap.java
@@ -1244,7 +1244,9 @@ public enum OnePObjectMap {
 	WAT_AUTHOR_SEARCH_PAGE_ORG_DROPDOWN_TEXT_XPATH("//p[text()='Select an organization where this author has published.']"),
 	WAT_AUTHOR_SEARCH_RESULTS_PAGE_PUBLICATIONS_DETAILS_AUTHOR_MORE_PUBLICATIONS_XPATH("//div[@class='wat-search-results-meta-publications-years-journals']/child::div "),
 	WAT_LOGO_XPATH("//div[@class='wui-platform-header__nav-content']/a"),
-	WAT_WEB_PAGE_LOGO_XPATH("//div[@class='row grid-start']/a/img")
+	WAT_WEB_PAGE_LOGO_XPATH("//div[@class='row grid-start']/a/img"),
+	WAT_PROFILE_NAME_XPATH("//div[@class='wui-subtitle wui-profile-flyout__name ng-binding']"),
+	WAT_PROFILE_PAGE_PROFILE_TEXT_XPATH("//h2[text()='Profile']")
 	
 	
 	;
