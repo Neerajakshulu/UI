cc0827d34b96fbe163df6866acaaa0aa02cda026
Chinna Putha <chinna.putha@thomsonreuters.com>
2017-10-10 14:52:38 +0530

    profile script fix

--- src/test/java/pages/HeaderFooterLinksPage.java
+++ src/test/java/pages/HeaderFooterLinksPage.java
@@ -47,7 +47,7 @@ public class HeaderFooterLinksPage extends TestBase {
 				}
 			}
 
-			else if (headerFooterLinks[i].equalsIgnoreCase("Terms of Use")){ 
+			else if (headerFooterLinks[i].equalsIgnoreCase("Terms of Use")){   
 				pf.getBrowserActionInstance(ob).click(OnePObjectMap.HOME_PROJECT_NEON_PROFILE_TERMS_OF_USE_LINK);
 				pf.getBrowserWaitsInstance(ob).waitUntilText(
 						"These Terms of Use shall govern your use of this online service and associated applications","Last updated");
