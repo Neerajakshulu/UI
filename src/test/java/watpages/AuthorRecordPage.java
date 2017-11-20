ee1a91008a57bd0044604c82ca0feac99eb87dd5
Chinna Putha <chinna.putha@thomsonreuters.com>
2017-11-20 15:29:22 +0530

    WAT new scripts

--- src/test/java/watpages/AuthorRecordPage.java
+++ src/test/java/watpages/AuthorRecordPage.java
@@ -1,5 +1,6 @@
 package watpages;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
 
 import com.relevantcodes.extentreports.ExtentTest;
@@ -17,6 +18,8 @@ import util.OnePObjectMap;
  */
 public class AuthorRecordPage extends TestBase {
 	boolean isDefaultAvatarPresent=false;
+	String metaTitle;
+	String metaOrg;
 
 	public AuthorRecordPage(WebDriver ob) {
 		this.ob = ob;
@@ -30,9 +33,9 @@ public class AuthorRecordPage extends TestBase {
 	public void waitForAuthorRecordPage(ExtentTest test) throws Exception{
 			pf.getBrowserWaitsInstance(ob).waitUntilText("Search Results");
 			pf.getBrowserWaitsInstance(ob).waitUntilText(
-					"The following details are available for this author. This author record is algorithmically generated and may not be complete. All information is derived from the publication metadata.");
-			pf.getBrowserWaitsInstance(ob).waitUntilText("Future iterations of author search will allow you to claim and edit your own profile to create a complete and accurate record of your work. ");
-			pf.getBrowserWaitsInstance(ob).waitUntilText("publication in Web of Science");
+					"The following details are available for this author.","This author record is algorithmically generated and may not be complete.","All information is derived from the publication metadata.");
+			pf.getBrowserWaitsInstance(ob).waitUntilText("Future iterations of author search will allow you to claim and","edit your own profile to create a complete and accurate record of your work.");
+			pf.getBrowserWaitsInstance(ob).waitUntilText("in Web of Science","Sorted by");
 			test.log(LogStatus.INFO, "User navigated to Author Record page");
 	}
 	
@@ -47,7 +50,7 @@ public class AuthorRecordPage extends TestBase {
     }
 	
 	/**
-	 * Method for click Search Results tab
+	 * Method for default avatar 
 	 * @param test
 	 * @throws Exception
 	 */
@@ -55,6 +58,33 @@ public class AuthorRecordPage extends TestBase {
 		isDefaultAvatarPresent=pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.WAT_AUTHOR_RECORD_DEFAULT_AVATAR_CSS).isDisplayed();
 		if(!isDefaultAvatarPresent){
 			throw new Exception("No Default Avatar/Author Profile Pic is not displayed in Author Record page");
+		}
+    }
+	
+	
+	/**
+	 * Method for author record meta title 
+	 * @param test
+	 * @throws Exception
+	 */
+	public void authorRecordMetaTitle() throws Exception{
+		metaTitle=pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.WAT_AUTHOR_RECORD_META_TITLE_CSS).getText();
+		String[] title=metaTitle.split(" ");
+		if(!(title.length>=2)){
+			throw new Exception("Profile Title in Author Record page should the form form First name, last name");
+		}
+    }
+	
+	
+	/**
+	 * Method for author record meta affiliation/organization 
+	 * @param test
+	 * @throws Exception
+	 */
+	public void authorRecordMetaOrganization() throws Exception{
+		metaOrg=pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.WAT_AUTHOR_RECORD_META_AFFILIATION_CSS).getText();
+		if(!StringUtils.isEmpty(metaOrg)){
+			throw new Exception("Profile metadata doesn't have any Organization");
 		}
     }
 	
