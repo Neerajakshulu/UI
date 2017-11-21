5979bf4a60eba79b9d770d70704a5c11ffdae21b
Krishnaraja Upadhyaya <krishnaraja.upadhyaya@thomsonreuters.com>
2017-11-21 16:51:47 +0530

    Adds WAT71 testscript

---
+++ src/test/java/wat/WAT71.java
@@ -0,0 +1,127 @@
+package wat;
+
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import base.TestBase;
+import util.ExtentManager;
+import util.OnePObjectMap;
+
+
+/**
+ * Class to Verify that until a minimum of 2 records are selected in author search results page, the Combine button should be disabled.
+ * 
+ * @author UC225218
+ *
+ */
+public class WAT71 extends TestBase {
+
+	static int status = 1;
+
+	/**
+	 * Method to displaying JIRA ID's for test case in specified path of Extent Reports
+	 * 
+	 * @throws Exception, When Something unexpected
+	 */
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		extent = ExtentManager.getReporter(filePath);
+		rowData = testcase.get(this.getClass().getSimpleName());
+		test = extent.startTest(rowData.getTestcaseId(), rowData.getTestcaseDescription()).assignCategory("WAT");
+	}
+
+	/**
+	 * Method for login into WAT application using TR ID
+	 * 
+	 * @throws Exception, When WAT Login is not done
+	 */
+	@Test
+	@Parameters({"username", "password"})
+	public void testLoginWATApp(String username,
+			String password) throws Exception {
+
+		boolean testRunmode = getTestRunMode(rowData.getTestcaseRunmode());
+		boolean master_condition = suiteRunmode && testRunmode;
+		logger.info("checking master condition status-->" + this.getClass().getSimpleName() + "-->" + master_condition);
+
+		if (!master_condition) {
+			status = 3;
+			test.log(LogStatus.SKIP,
+					"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
+			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
+		}
+
+		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts ");
+
+		try {
+			openBrowser();
+			clearCookies();
+			maximizeWindow();
+			test.log(LogStatus.INFO, "Logging into WAT Applicaton using valid WAT Entitled user ");
+			ob.navigate().to(host + CONFIG.getProperty("appendWATAppUrl"));
+			pf.getLoginTRInstance(ob).loginToWAT(username, password, test);
+			pf.getSearchAuthClusterPage(ob).validateAuthorSearchPage(test);
+		} catch (Throwable t) {
+			logFailureDetails(test, t, "Login Fail", "login_fail");
+			pf.getBrowserActionInstance(ob).closeBrowser();
+		}
+
+	}
+
+	/**
+	 * Method to Verify that until a minimum of 2 records are selected in author search results page, the Combine button should be disabled.
+	 * 
+	 * @param orcid
+	 * @throws Exception, When Something unexpected
+	 */
+	@Test(dependsOnMethods = {"testLoginWATApp"})
+	@Parameters({ "LastName", "CountryName1", "CountryName2","OrgName1", "OrgName2" })
+	public void CombineButtonminimumReq(String LastName, String CountryName1,String CountryName2,
+			String OrgName1,String OrgName2) throws Exception {
+		try {
+			pf.getSearchAuthClusterPage(ob).searchAuthorClusterOnlyLastName(LastName, CountryName1,CountryName2,
+					OrgName1, OrgName2,test);
+			pf.getBrowserWaitsInstance(ob).waitUntilElementIsDisplayed(OnePObjectMap.WAT_AUTHOR_SEARCH_RESULT_CARD1_XPATH, 5);
+			pf.getBrowserActionInstance(ob).click(OnePObjectMap.WAT_AUTHOR_SEARCH_RESULT_CARD1_XPATH);
+			test.log(LogStatus.INFO, "Author card 1 is selected.");
+			Assert.assertEquals(pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.WAT_COMBINE_BUTTON_XPATH).getAttribute("disabled"), "true", "Combine button is not in disabled state");
+			test.log(LogStatus.INFO, "Combine button is in disabled state");
+			pf.getBrowserWaitsInstance(ob).waitUntilElementIsDisplayed(OnePObjectMap.WAT_AUTHOR_SEARCH_RESULT_CARD2_XPATH, 5);
+			pf.getBrowserActionInstance(ob).click(OnePObjectMap.WAT_AUTHOR_SEARCH_RESULT_CARD2_XPATH);
+			test.log(LogStatus.INFO, "Author card 2 is selected.");
+			
+			Assert.assertTrue(pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.WAT_COMBINE_BUTTON_XPATH).isEnabled());
+			test.log(LogStatus.INFO, "Combine button is in enabled state");
+			test.log(LogStatus.PASS, "Combine button functionality is working as expected");
+		} catch (Throwable t) {
+			t.printStackTrace();
+			logFailureDetails(test, t, "Combine button not disabled", "author_combine_Btn_fail");
+			pf.getBrowserActionInstance(ob).closeBrowser();
+		}
+	}
+
+	/**
+	 * updating Extent Report with test case status whether it is PASS or FAIL or SKIP
+	 */
+	@AfterTest
+	public void reportTestResult() {
+		pf.getBrowserActionInstance(ob).closeBrowser();
+		extent.endTest(test);
+
+		/*
+		 * if (status == 1) TestUtil.reportDataSetResult(profilexls, "Test Cases", TestUtil.getRowNum(profilexls,
+		 * this.getClass().getSimpleName()), "PASS"); else if (status == 2) TestUtil.reportDataSetResult(profilexls,
+		 * "Test Cases", TestUtil.getRowNum(profilexls, this.getClass().getSimpleName()), "FAIL"); else
+		 * TestUtil.reportDataSetResult(profilexls, "Test Cases", TestUtil.getRowNum(profilexls,
+		 * this.getClass().getSimpleName()), "SKIP");
+		 */
+
+	}
+
+}
