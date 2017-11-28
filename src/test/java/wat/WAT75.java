24ec692a43972f038810fd67c57e0bc445b23ac4
Krishnaraja Upadhyaya <krishnaraja.upadhyaya@thomsonreuters.com>
2017-11-28 11:14:51 +0530

    Adds WAT75 testscript

---
+++ src/test/java/wat/WAT75.java
@@ -0,0 +1,118 @@
+package wat;
+
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import base.TestBase;
+import util.ExtentManager;
+import util.OnePObjectMap;
+
+
+/**
+ * Class to Verify that System toggles the "Select all" option to "Deselect all" when user clicks on the Select all option.
+ * 
+ * @author UC225218
+ *
+ */
+public class WAT75 extends TestBase {
+
+	static int status = 1;
+
+	/**
+	 * Method to displaying JIRA ID's for test case in specified path of Extent Reports
+	 * 
+	 * @throws Exception, When Something unexpected
+	 */
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		extent = ExtentManager.getReporter(filePath);
+		rowData = testcase.get(this.getClass().getSimpleName());
+		test = extent.startTest(rowData.getTestcaseId(), rowData.getTestcaseDescription()).assignCategory("WAT");
+	}
+
+	/**
+	 * Method for login into WAT application using TR ID
+	 * 
+	 * @throws Exception, When WAT Login is not done
+	 */
+	@Test
+	@Parameters({"username", "password"})
+	public void testLoginWATApp(String username,
+			String password) throws Exception {
+
+		boolean testRunmode = getTestRunMode(rowData.getTestcaseRunmode());
+		boolean master_condition = suiteRunmode && testRunmode;
+		logger.info("checking master condition status-->" + this.getClass().getSimpleName() + "-->" + master_condition);
+
+		if (!master_condition) {
+			status = 3;
+			test.log(LogStatus.SKIP,
+					"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
+			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
+		}
+
+		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts ");
+
+		try {
+			openBrowser();
+			clearCookies();
+			maximizeWindow();
+			test.log(LogStatus.INFO, "Logging into WAT Applicaton using valid WAT Entitled user ");
+			ob.navigate().to(host + CONFIG.getProperty("appendWATAppUrl"));
+			pf.getLoginTRInstance(ob).loginToWAT(username, password, test);
+			pf.getSearchAuthClusterPage(ob).validateAuthorSearchPage(test);
+		} catch (Throwable t) {
+			logFailureDetails(test, t, "Login Fail", "login_fail");
+			pf.getBrowserActionInstance(ob).closeBrowser();
+		}
+
+	}
+
+	/**
+	 * Method to Verify that System toggles the "Select all" option to "Deselect all" when user clicks on the Select all option.
+	 * 
+	 * @param orcid
+	 * @throws Exception, When Something unexpected
+	 */
+	@Test(dependsOnMethods = {"testLoginWATApp"})
+	@Parameters({ "LastName", "CountryName1", "CountryName2","OrgName1", "OrgName2" })
+	public void VerifySelectAllToDeSelectall(String LastName, String CountryName1,String CountryName2,
+			String OrgName1,String OrgName2) throws Exception {
+		try {
+			pf.getSearchAuthClusterPage(ob).searchAuthorClusterOnlyLastName(LastName, CountryName1,CountryName2,
+					OrgName1, OrgName2,test);
+			pf.getSearchAuthClusterResultsPage(ob).selectAllAuthorCard(test);
+			pf.getBrowserWaitsInstance(ob).waitUntilElementIsDisplayed(OnePObjectMap.WAT_DESELECT_ALL_LINK_XPATH, 5);
+			if(pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.WAT_DESELECT_ALL_LINK_XPATH).isDisplayed())
+				test.log(LogStatus.PASS, "DeSelect all link is displayed after clicking on Select all.");
+		} catch (Throwable t) {
+			t.printStackTrace();
+			logFailureDetails(test, t, "Author DeSelection Fail", "author_deselection_fail");
+			pf.getBrowserActionInstance(ob).closeBrowser();
+		}
+	}
+
+	/**
+	 * updating Extent Report with test case status whether it is PASS or FAIL or SKIP
+	 */
+	@AfterTest
+	public void reportTestResult() {
+		pf.getBrowserActionInstance(ob).closeBrowser();
+		extent.endTest(test);
+
+		/*
+		 * if (status == 1) TestUtil.reportDataSetResult(profilexls, "Test Cases", TestUtil.getRowNum(profilexls,
+		 * this.getClass().getSimpleName()), "PASS"); else if (status == 2) TestUtil.reportDataSetResult(profilexls,
+		 * "Test Cases", TestUtil.getRowNum(profilexls, this.getClass().getSimpleName()), "FAIL"); else
+		 * TestUtil.reportDataSetResult(profilexls, "Test Cases", TestUtil.getRowNum(profilexls,
+		 * this.getClass().getSimpleName()), "SKIP");
+		 */
+
+	}
+
+}
