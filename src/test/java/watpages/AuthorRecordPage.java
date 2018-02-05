0eab800b0db8303e0e62f26acd1eb21ca17de756
Chinna Putha <chinna.putha@thomsonreuters.com>
2018-02-05 15:06:54 +0530

    WAT Regression script fixes

--- src/test/java/watpages/AuthorRecordPage.java
+++ src/test/java/watpages/AuthorRecordPage.java
@@ -41,7 +41,7 @@ public class AuthorRecordPage extends TestBase {
 			pf.getBrowserWaitsInstance(ob).waitUntilText("Search Results");
 			pf.getBrowserWaitsInstance(ob).waitUntilText(
 					"The following details are available for this author.","This author record is algorithmically generated and may not be complete.","All information is derived from the publication metadata.");
-			pf.getBrowserWaitsInstance(ob).waitUntilText("Future iterations of author search will allow you to claim and","edit your own profile to create a complete and accurate record of your work.");
+			//pf.getBrowserWaitsInstance(ob).waitUntilText("Future iterations of author search will allow you to claim and","edit your own profile to create a complete and accurate record of your work.");
 			pf.getBrowserWaitsInstance(ob).waitUntilText("in Web of Science","Sorted by");
 			test.log(LogStatus.INFO, "User navigated to Author Record page");
 	}
