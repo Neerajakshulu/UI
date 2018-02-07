d89dbbd9c4ef7e7d1efa484f68f781588b1c5438
Chinna Putha <chinna.putha@thomsonreuters.com>
2018-02-07 16:37:00 +0530

    WAT new script implementation

--- src/test/java/util/OnePObjectMap.java
+++ src/test/java/util/OnePObjectMap.java
@@ -1320,7 +1320,8 @@ public enum OnePObjectMap {
 	WAT_AUTHOR_RECORD_PAGE_ORGANISATION_NAME_XPATH("//ul[@class='nav nav-tabs']/li[3]/a"),
 	WAT_AUTHOR_RECORD_PAGE_ORGANISATION_TAB_STATUS_XPATH("//ul[@class='nav nav-tabs']/li[3]/a/ancestor::li"),
 	WAT_AUTHOR_RECORD_PAGE_ALTERNATIVE_NAME_TAB_STATUS_XPATH("//ul[@class='nav nav-tabs']/li[2]/a/ancestor::li"),
-	
+	WAT_AUTHOR_RECORD_PAGE_METRICS_XPATH("//ul[@class='nav nav-tabs']/li[1]/a"),
+	WAT_AUTHOR_RECORD_PAGE_METRICS_TAB_ITEMS_XPATH("//div[@class='wat-author-record__metrics-item-column'][$index]/div"),
 	
 	
 	WAT_SUGGEST_UPDATE_BTN_XPATH("//button[@class='wui-btn wui-btn--primary' and contains(text(),'Suggest updates')]"),
