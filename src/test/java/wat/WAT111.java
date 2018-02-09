1b48f92936d6b100942fcff07ed6bf03da3681e4
Krishnaraja Upadhyaya <krishnaraja.upadhyaya@thomsonreuters.com>
2018-02-09 16:13:11 +0530

    Adds WAT111 and related changes

---
+++ src/test/java/wat/WAT111.java
@@ -0,0 +1,134 @@
+package wat;
+
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import base.TestBase;
+import util.ExtentManager;
+import util.OnePObjectMap;
+
+/**
+ * Class for Verify that Add alternative name icon is gray until the name has been entered in each name module
+ * 
+ * @author UC225218
+ *
+ */
+
+public class WAT111 extends TestBase {
+
+	static int status = 1;
+	static String wos_title = "Web of Science: Author search";
+	static String sar_labs_text = "SaR Labs";
+	static String Orcid_button_text = "ORCiD search";
+	
+	/**
+	 * Method for displaying JIRA ID's for test case in specified path of Extent
+	 * Reports
+	 * 
+	 * @throws Exception,
+	 *             When Something unexpected
+	 */
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		extent = ExtentManager.getReporter(filePath);
+		rowData = testcase.get(this.getClass().getSimpleName());
+		test = extent.startTest(rowData.getTestcaseId(), rowData.getTestcaseDescription()).assignCategory("WAT");
+	}
+
+	/**
+	 * Method for login into WAT application using Steam ID
+	 * 
+	 * @throws Exception,
+	 *             When WAT Login is not done
+	 */
+	@Test
+	@Parameters({ "username", "password" })
+	public void testLoginWATApp(String username, String password) throws Exception {
+
+		boolean testRunmode = getTestRunMode(rowData.getTestcaseRunmode());
+		boolean master_condition = suiteRunmode && testRunmode;
+		logger.info("checking master condition status-->" + this.getClass().getSimpleName() + "-->" + master_condition);
+
+		if (!master_condition) {
+			status = 3;
+			test.log(LogStatus.SKIP,
+					"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
+			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
+		}
+
+		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts.... ");
+
+		try {
+			openBrowser();
+			clearCookies();
+			maximizeWindow();
+			test.log(LogStatus.INFO, "Login to WAT Applicaton using valid WAT Entitled user ");
+			ob.navigate().to(host + CONFIG.getProperty("appendWATAppUrl"));
+			pf.getLoginTRInstance(ob).loginToWAT(username, password, test);
+
+		} catch (Throwable t) {
+			logFailureDetails(test, t, "Login Fail", "login_fail");
+			pf.getBrowserActionInstance(ob).closeBrowser();
+		}
+
+	}
+
+	/**
+	 * Method to Verify that Add alternative name icon is gray until the name has been entered in each name module
+	 * 
+	 * @throws Exception,
+	 *             When Something unexpected
+	 * 
+	 */
+	@Test(dependsOnMethods = { "testLoginWATApp" })
+	@Parameters({ "LastName"})
+	public void testAddAltNameFunctionality(String LastName) throws Exception {
+
+		// NOTE - Assertion ERRORs are caught in EXCEPTION block just for
+		// Reporting purpose
+		try {
+		
+			Assert.assertEquals(pf.getBrowserActionInstance(ob)
+						.getElement(OnePObjectMap.WAT_ADD_ALT_NAME_BTN_TEXT_XPATH).getAttribute("disabled"), "true","Add alternate button is not disabled in the beginning");
+			pf.getBrowserActionInstance(ob).click(OnePObjectMap.WAT_AUTHOR_LASTNAME_XPATH);
+			pf.getSearchAuthClusterPage(ob).enterAuthorLastName(LastName, test);
+			if(pf.getBrowserActionInstance(ob)
+					.getElement(OnePObjectMap.WAT_ADD_ALT_NAME_BTN_TEXT_XPATH).getAttribute("disabled")!=null)
+			Assert.assertTrue(false,"Add alternate button is disabled after enetring Lastname/First name");
+			test.log(LogStatus.PASS, "Add alternative name icon is gray until the name has been entered in each name module");
+				pf.getBrowserActionInstance(ob).closeBrowser();
+		} catch (Exception e) {
+				test.log(LogStatus.FAIL, "Add alternative name icon is not gray until the name has been entered in each name module");
+				logFailureDetails(test, e, "Add alternative name icon is not gray until the name has been entered in each name module", "Add_alt_btn_error");
+				pf.getBrowserActionInstance(ob).closeBrowser();
+		}
+	}
+
+	/**
+	 * updating Extent Report with test case status whether it is PASS or FAIL
+	 * or SKIP
+	 */
+	@AfterTest
+	public void reportTestResult() {
+
+		extent.endTest(test);
+
+		/*
+		 * if (status == 1) TestUtil.reportDataSetResult(profilexls,
+		 * "Test Cases", TestUtil.getRowNum(profilexls,
+		 * this.getClass().getSimpleName()), "PASS"); else if (status == 2)
+		 * TestUtil.reportDataSetResult(profilexls, "Test Cases",
+		 * TestUtil.getRowNum(profilexls, this.getClass().getSimpleName()),
+		 * "FAIL"); else TestUtil.reportDataSetResult(profilexls, "Test Cases",
+		 * TestUtil.getRowNum(profilexls, this.getClass().getSimpleName()),
+		 * "SKIP");
+		 */
+
+	}
+}
