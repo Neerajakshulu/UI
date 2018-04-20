ad76e3c4c8497600b8f18072cb13af0503e5fbb5
Mahesh.Morsu <Mahesh.Morsu@thomsonreuters.com>
2018-04-20 12:14:15 +0530

    Changed log status test base class

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -960,7 +960,7 @@ public class TestBase {
 		} catch (Throwable t) {
 			t.printStackTrace();
 			test.log(
-					LogStatus.INFO,
+					LogStatus.FAIL,
 					"Snapshot below: "
 							+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName()
 									+ "_user_not_registered")));// screenshot
@@ -992,7 +992,7 @@ public class TestBase {
 		} catch (Throwable t) {
 			t.printStackTrace();
 			test.log(
-					LogStatus.INFO,
+					LogStatus.FAIL,
 					"Snapshot below: "
 							+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName()
 									+ "_user_not_registered")));// screenshot
@@ -2130,7 +2130,7 @@ public class TestBase {
 		} catch (Throwable t) {
 			t.printStackTrace();
 			test.log(
-					LogStatus.INFO,
+					LogStatus.FAIL,
 					"Snapshot below: "
 							+ test.addScreenCapture(captureScreenshot(this.getClass().getSimpleName()
 									+ "_user_not_registered")));// screenshot
