1b18adbe3918f7036514c2fdb5540af375bd4670
Krishnaraja Upadhyaya <krishnaraja.upadhyaya@thomsonreuters.com>
2018-06-05 15:47:13 +0530

    Adds WAT-148 testscript and related changes

---
+++ src/test/java/wat/WAT148.java
@@ -0,0 +1,159 @@
+package wat;
+
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import base.TestBase;
+import util.ExtentManager;
+import util.OnePObjectMap;
+
+/**
+ * Class to Verify that for a Single author the "Total Citing Publications"
+ * value will be less than or equal to the "Total Citations" value.
+ * 
+ * @author UC225218
+ *
+ */
+public class WAT148 extends TestBase {
+
+	static int status = 1;
+	int i;
+	String text = "H-INDEX";
+
+	/**
+	 * Method to displaying JIRA ID's for test case in specified path of Extent
+	 * Reports
+	 * 
+	 * @throws Exception,
+	 *             When Something unexpected
+	 */
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		extent = ExtentManager.getReporter(filePath);
+		rowData = testcase.get(this.getClass().getSimpleName());
+		test = extent.startTest(rowData.getTestcaseId(), rowData.getTestcaseDescription()).assignCategory("WAT");
+	}
+
+	/**
+	 * Method for login into WAT application using TR ID
+	 * 
+	 * @throws Exception,
+	 *             When WAT Login is not done
+	 */
+	@Test
+	@Parameters({ "username", "password" })
+	public void testLoginWATApp(String username, String password) throws Exception {
+
+		boolean testRunmode = getTestRunMode(rowData.getTestcaseRunmode());
+		boolean master_condition = suiteRunmode && testRunmode;
+		logger.info("checking master condition status-->" + this.getClass().getSimpleName() + "-->" + master_condition);
+
+		if (!master_condition) {
+			status = 3;
+			test.log(LogStatus.SKIP,
+					"Skipping test case " + this.getClass().getSimpleName() + " as the run mode is set to NO");
+			throw new SkipException("Skipping Test Case" + this.getClass().getSimpleName() + " as runmode set to NO");// reports
+		}
+
+		test.log(LogStatus.INFO, this.getClass().getSimpleName() + " execution starts ");
+
+		try {
+			openBrowser();
+			clearCookies();
+			maximizeWindow();
+			test.log(LogStatus.INFO, "Logging into WAT Applicaton using valid WAT Entitled user ");
+			ob.navigate().to(host + CONFIG.getProperty("appendWATAppUrl"));
+			pf.getLoginTRInstance(ob).loginToWAT(username, password, test);
+			pf.getSearchAuthClusterPage(ob).validateAuthorSearchPage(test);
+
+		} catch (Throwable t) {
+			logFailureDetails(test, t, "Login Fail", "login_fail");
+			pf.getBrowserActionInstance(ob).closeBrowser();
+		}
+
+	}
+
+	/**
+	 * Method for Search Author cluster Results
+	 * 
+	 * @param lastName,countryName,orgName
+	 * @throws Exception,
+	 *             When Something unexpected
+	 */
+	@Test(dependsOnMethods = { "testLoginWATApp" })
+	@Parameters({ "LastName", "CountryName1", "CountryName2", "OrgName1", "OrgName2" })
+	public void searchAuthorCluster(String LastName, String CountryName1, String CountryName2, String OrgName1,
+			String OrgName2) throws Exception {
+		try {
+			test.log(LogStatus.INFO, "Entering author name... ");
+			pf.getSearchAuthClusterPage(ob).searchAuthorClusterOnlyLastName(LastName, CountryName1, CountryName2,
+					OrgName1, OrgName2, test);
+			test.log(LogStatus.INFO, "Author Search Results are displayed");
+		} catch (Throwable t) {
+			logFailureDetails(test, t, "Author Search Results are not displayed", "Search_Fail");
+			pf.getBrowserActionInstance(ob).closeBrowser();
+		}
+	}
+
+	/**
+	 * Method to Verify that for a Single author the "Total Citing
+	 * Publications" value will be less than or equal to the "Total Citations"
+	 * value.
+	 * 
+	 * @throws Exception,
+	 *             When Something unexpected
+	 */
+	@Test(dependsOnMethods = { "searchAuthorCluster" })
+	public void testMetricsValue() throws Exception {
+		try {
+			test.log(LogStatus.INFO, "Clicking first author card");
+			pf.getBrowserActionInstance(ob).getElement(OnePObjectMap.WAT_AUTHOR_SEARCH_RESULT_FIRST_CARD_XPATH).click();
+			pf.getBrowserWaitsInstance(ob)
+					.waitUntilElementIsDisplayed(OnePObjectMap.WAT_AUTHOR_RECORD_DEFAULT_AVATAR_CSS);
+			if (Integer.parseInt(pf.getBrowserActionInstance(ob)
+					.getElement(OnePObjectMap.WAT_AUTHOR_RECORD_METADATA_CITINGARTICLES_VALUE_XPATH).getText()
+					.replaceAll(",", "")) <= Integer
+							.parseInt(pf.getBrowserActionInstance(ob)
+									.getElement(OnePObjectMap.WAT_AUTHOR_RECORD_METADATA_SUMOFTIMESCITED_VALUE_XPATH)
+									.getText().replaceAll(",", "")))
+				test.log(LogStatus.PASS,
+						"Total Citing Publications value is less than or equal to the Total Citations value.");
+			else
+				throw new Exception(
+						"Total Citing Publications value is not less than or equal to the Total Citations value.");
+
+			pf.getBrowserActionInstance(ob).closeBrowser();
+		} catch (Throwable t) {
+			logFailureDetails(test, t, "Metrics in Single author page is not displayed as expected",
+					"Metric_display_issue");
+			pf.getBrowserActionInstance(ob).closeBrowser();
+		}
+	}
+
+	/**
+	 * updating Extent Report with test case status whether it is PASS or FAIL
+	 * or SKIP
+	 */
+	@AfterTest
+	public void reportTestResult() {
+		extent.endTest(test);
+
+		/*
+		 * if (status == 1) TestUtil.reportDataSetResult(profilexls,
+		 * "Test Cases", TestUtil.getRowNum(profilexls,
+		 * this.getClass().getSimpleName()), "PASS"); else if (status == 2)
+		 * TestUtil.reportDataSetResult(profilexls, "Test Cases",
+		 * TestUtil.getRowNum(profilexls, this.getClass().getSimpleName()),
+		 * "FAIL"); else TestUtil.reportDataSetResult(profilexls, "Test Cases",
+		 * TestUtil.getRowNum(profilexls, this.getClass().getSimpleName()),
+		 * "SKIP");
+		 */
+
+	}
+
+}
