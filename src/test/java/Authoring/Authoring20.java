f45f1de427b1ad455323bb18b4bc32ab5bd6728f
MunnaModi <munnakumar.modi@clarivate.com>
2018-07-19 16:31:02 +0530

    Updated Authoring Tests and comments searched post related tests.

--- src/test/java/Authoring/Authoring20.java
+++ src/test/java/Authoring/Authoring20.java
@@ -65,7 +65,7 @@ public class Authoring20 extends TestBase {
 			ob.navigate().to(host);
 			loginAs("USERNAME16", "PASSWORD16");
 			String PROFILE_NAME = LOGIN.getProperty("PROFILE16");
-			pf.getHFPageInstance(ob).searchForText("Physics");
+			pf.getHFPageInstance(ob).searchForText("chemistry");
 
 			pf.getSearchResultsPageInstance(ob).searchForArticleWithComments();
 			pf.getpostRVPageInstance(ob).loadComments();
