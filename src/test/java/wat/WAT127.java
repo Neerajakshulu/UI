2f6ee0f2f0a82fe83b671125ed1d46d5703e2610
Krishnaraja-upadhyaya <krishnaraja.upadhyaya@thomsonreuters.com>
2018-10-30 13:04:04 +0530

    WAT-127 Fixed

--- src/test/java/wat/WAT127.java
+++ src/test/java/wat/WAT127.java
@@ -66,9 +66,8 @@ public class WAT127 extends TestBase {
 			openBrowser();
 			clearCookies();
 			maximizeWindow();
-			test.log(LogStatus.INFO, "Login to WAT Applicaton using valid WAT Entitled user ");
-			ob.navigate().to(host + CONFIG.getProperty("appendWATAppUrl"));
-			pf.getLoginTRInstance(ob).loginToWAT(username, password, test);
+			test.log(LogStatus.INFO, "Logging into WAT Applicaton through WoS Application.");
+			pf.getWatPageInstance(ob).loginToWOSWAT(test);
 
 		} catch (Throwable t) {
 			logFailureDetails(test, t, "Login Fail", "login_fail");
